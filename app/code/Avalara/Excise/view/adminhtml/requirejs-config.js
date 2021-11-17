cae7614b64a0ff74024d3b3c5cd7003fbaf4aaa5
karangar.meghnathi <karangar.meghnathi@brainvire.com>
2021-11-17 16:42:07 +0530

    remove avalara extension from code and added via composer

--- app/code/Avalara/Excise/view/adminhtml/requirejs-config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    map: {
-        '*': {
-            companyCode: 'Avalara_Excise/js/form/field/company-code',
-            'Magento_Customer/template/default-address':'Avalara_Excise/template/default-address'
-        }
-    }
-};
