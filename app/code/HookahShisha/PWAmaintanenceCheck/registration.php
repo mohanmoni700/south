65932d19e14cf3bd37b4bda3b3675d1deba3c685
Ashwin Shahi <ashwin@corra.com>
2022-01-07 13:20:22 +0530

    ALFA-531: reinstalled PWA maintenance module

--- app/code/HookahShisha/PWAmaintanenceCheck/registration.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @author C O R R A
- */
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'HookahShisha_PWAmaintanenceCheck', __DIR__);
