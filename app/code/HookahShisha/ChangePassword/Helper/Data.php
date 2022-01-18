9425f2bf0cdb4052407dfb7ce370b2f61213bff7
karangar.meghnathi <karangar.meghnathi@brainvire.com>
2022-01-18 20:05:48 +0530

    feature/B2BHW-43 : solve out the session realted changes

--- app/code/HookahShisha/ChangePassword/Helper/Data.php
+++ app/code/HookahShisha/ChangePassword/Helper/Data.php
@@ -4,16 +4,21 @@ namespace HookahShisha\ChangePassword\Helper;
 class Data extends \Magento\Framework\App\Helper\AbstractHelper
 {
     protected $_customerSession;
+    protected $httpContext;
 
     public function __construct(
-        \Magento\Customer\Model\Session $session
+        \Magento\Customer\Model\Session $session,
+        \Magento\Framework\App\Http\Context $httpContext
     ) {
         $this->_customerSession = $session;
+        $this->httpContext = $httpContext;
     }
 
     public function CustomerLogin()
     {
-        if ($this->_customerSession->isLoggedIn()) {
+        $isLoggedIn = $this->httpContext->getValue(\Magento\Customer\Model\Context::CONTEXT_AUTH);
+
+        if ($isLoggedIn) {
             return $this->_customerSession;
         }
     }
