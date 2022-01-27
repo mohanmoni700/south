685d13f34a988173618d0c4275f1ff5aabef79b1
karangar.meghnathi <karangar.meghnathi@brainvire.com>
2022-01-27 16:48:43 +0530

    ALFA-256 : Avalara extension Plugin error

--- app/code/HookahShisha/Avalara/Plugin/DefaultRendererPlugin.php
+++ app/code/HookahShisha/Avalara/Plugin/DefaultRendererPlugin.php
@@ -35,7 +35,7 @@ class DefaultRendererPlugin
     /**
      * Retrieve rendered column html content
      *
-     * @param DefaultRenderer $defaultRenderer
+     * @param Magento\Sales\Block\Adminhtml\Order\View\Items\Renderer\DefaultRenderer $defaultRenderer
      * @param \Closure $proceed
      * @param \Magento\Framework\DataObject|Item $item
      * @param string $column
@@ -55,31 +55,19 @@ class DefaultRendererPlugin
             $currency = $this->CurrencyFactory->load($item->getOrder()->getOrderCurrencyCode());
             $currencySymbol = $currency->getCurrencySymbol();
             $html = $currencySymbol . number_format($item->getTaxAmount(), 2);
-            if (!empty($item->getExciseTax())) {
+            if (!empty($item->getExciseTax()) && !empty($item->getSalesTax())) {
                 $html .= "<br/>Excise Tax - " . $currencySymbol . $item->getExciseTax();
-            }
-            if (!empty($item->getSalesTax())) {
                 $html .= "<br/>Sales Tax - " . $currencySymbol . $item->getSalesTax();
             }
             $result = $html;
         } elseif ($column == 'tax-percent') {
             $itemTotal = (($item->getPrice() * $item->getQtyOrdered()) - $item->getDiscountAmount());
-            if ($itemTotal) {
-                $orderTaxRate = number_format(($item->getTaxAmount() * 100) / $itemTotal, 2);
-            } else {
-                $orderTaxRate = number_format(0, 2);
-            }
-            /*
-             * Removed default condition
-             * $orderTaxRate = number_format(($item->getTaxAmount()*100) / $itemTotal, 2);
-             */
-            $html = $orderTaxRate . "%";
+            $taxRate= ($itemTotal) ? number_format(($item->getTaxAmount() * 100) / $itemTotal, 2) : number_format(1, 2);
+            $html = $taxRate . "%";
 
-            if (!empty($item->getExciseTax())) {
+            if (!empty($item->getExciseTax()) && !empty($item->getSalesTax())) {
                 $exciseTaxRate = number_format(($item->getExciseTax() * 100) / $itemTotal, 2);
                 $html .= "<br/>Excise Tax - " . $exciseTaxRate . "%";
-            }
-            if (!empty($item->getSalesTax())) {
                 $salesTaxRate = number_format(($item->getSalesTax() * 100) / $itemTotal, 2);
                 $html .= "<br/>Sales Tax - " . $salesTaxRate . "%";
             }
@@ -91,7 +79,6 @@ class DefaultRendererPlugin
                 $result = $proceed($item, $column);
             }
         }
-
         return $result;
     }
 }
