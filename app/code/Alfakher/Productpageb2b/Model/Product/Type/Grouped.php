c17866215d07d2ee65067cd4b1a641149ff25172
bhavya.sheth <bhavya.sheth@brainvire.com>
2022-02-25 15:04:50 +0530

    products alphabetically on PDP page

---
+++ app/code/Alfakher/Productpageb2b/Model/Product/Type/Grouped.php
@@ -0,0 +1,34 @@
+<?php
+namespace Alfakher\Productpageb2b\Model\Product\Type;
+
+class Grouped extends \Magento\GroupedProduct\Model\Product\Type\Grouped
+{
+    public function getAssociatedProducts($product)
+    {
+        if (!$product->hasData($this->_keyAssociatedProducts)) {
+            $associatedProducts = [];
+
+            $this->setSaleableStatus($product);
+
+            $collection = $this->getAssociatedProductCollection(
+                $product
+            )->addAttributeToSelect(
+                ['name', 'price', 'special_price', 'special_from_date', 'special_to_date', 'tax_class_id']
+            )->addFilterByRequiredOptions()->addStoreFilter(
+                $this->getStoreFilter($product)
+            )->addAttributeToFilter(
+                'status',
+                ['in' => $this->getStatusFilters($product)]
+            );
+
+            $collection->setOrder('name', 'ASC');
+
+            foreach ($collection as $item) {
+                $associatedProducts[] = $item;
+            }
+
+            $product->setData($this->_keyAssociatedProducts, $associatedProducts);
+        }
+        return $product->getData($this->_keyAssociatedProducts);
+    }
+}
