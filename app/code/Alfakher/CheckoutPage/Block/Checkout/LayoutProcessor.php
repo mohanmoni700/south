fa84ad5b0de5bbf59b8fc42314664d931cc71940
bhavya.sheth <bhavya.sheth@brainvire.com>
2022-03-16 12:38:47 +0530

    address field changes

---
+++ app/code/Alfakher/CheckoutPage/Block/Checkout/LayoutProcessor.php
@@ -0,0 +1,18 @@
+<?php
+namespace Alfakher\CheckoutPage\Block\Checkout;
+
+class LayoutProcessor
+{
+    public function afterProcess(
+        \Magento\Checkout\Block\Checkout\LayoutProcessor $subject,
+        array $jsLayout
+    ) {
+        $jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']
+        ['children']['shippingAddress']['children']['shipping-address-fieldset']['children']['street']['children'][0]['label'] = __('Address Line 1');
+
+        $jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']
+        ['children']['shippingAddress']['children']['shipping-address-fieldset']['children']['street']['children'][1]['label'] = __('Address Line 2');
+
+        return $jsLayout;
+    }
+}
