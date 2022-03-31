971191fffd5dad5d39198492058f8aa3697de810
karangar.meghnathi <karangar.meghnathi@brainvire.com>
2022-04-01 02:39:37 +0530

    checking the condition product is available or not

--- app/code/Alfakher/GrossMargin/ViewModel/GrossMargin.php
+++ app/code/Alfakher/GrossMargin/ViewModel/GrossMargin.php
@@ -7,49 +7,49 @@ namespace Alfakher\GrossMargin\ViewModel;
  *
  * @author af_bv_op
  */
-class GrossMargin implements \Magento\Framework\View\Element\Block\ArgumentInterface
-{
-    const MODULE_ENABLE = "hookahshisha/gross_margin_group/gross_margin_enable";
+class GrossMargin implements \Magento\Framework\View\Element\Block\ArgumentInterface {
+	const MODULE_ENABLE = "hookahshisha/gross_margin_group/gross_margin_enable";
 
-    /**
-     * Constructor
-     *
-     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
-     */
-    public function __construct(
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
-    ) {
-        $this->scopeConfig = $scopeConfig;
-    }
+	/**
+	 * Constructor
+	 *
+	 * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+	 */
+	public function __construct(
+		\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+	) {
+		$this->scopeConfig = $scopeConfig;
+	}
 
-    /**
-     * Check if module is enable
-     *
-     * @param int $websiteId
-     */
-    public function isModuleEnabled($websiteId)
-    {
-        $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_WEBSITE;
-        return $this->scopeConfig->getValue(self::MODULE_ENABLE, $storeScope, $websiteId);
-    }
+	/**
+	 * Check if module is enable
+	 *
+	 * @param int $websiteId
+	 */
+	public function isModuleEnabled($websiteId) {
+		$storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_WEBSITE;
+		return $this->scopeConfig->getValue(self::MODULE_ENABLE, $storeScope, $websiteId);
+	}
 
-    /**
-     * Validate gross margin
-     *
-     * @param \Magento\Sales\Model\Order\Item $item
-     */
-    public function validateGrossMargin($item)
-    {
-        if ($item->getGrossMargin() <= 0) {
-            $cost = $item->getProduct()->getCost();
-            $price = $item->getPrice();
-            try {
-                $grossMargin = ($price - $cost) / $price * 100;
-                return number_format($grossMargin, 2, ".", "");
-            } catch (\Exception $e) {
-                return 0.00;
-            }
-        }
-        return $item->getGrossMargin();
-    }
+	/**
+	 * Validate gross margin
+	 *
+	 * @param \Magento\Sales\Model\Order\Item $item
+	 */
+	public function validateGrossMargin($item) {
+		if ($item->getGrossMargin() <= 0) {
+			$cost = 0;
+			if ($item->getProduct()) {
+				$cost = $item->getProduct()->getCost();
+			}
+			$price = $item->getPrice();
+			try {
+				$grossMargin = ($price - $cost) / $price * 100;
+				return number_format($grossMargin, 2, ".", "");
+			} catch (\Exception $e) {
+				return 0.00;
+			}
+		}
+		return $item->getGrossMargin();
+	}
 }
