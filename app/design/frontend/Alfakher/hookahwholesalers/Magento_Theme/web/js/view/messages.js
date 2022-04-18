4d4cf47738faa8185feaca37b4930a0902c10310
Meet Patel <mitkumar.patel@brainvire.com>
2022-04-18 11:59:01 +0530

    B2BHW-733,B2BHW-729,B2BHW-330 : Fix the message notification issue

--- app/design/frontend/Alfakher/hookahwholesalers/Magento_Theme/web/js/view/messages.js
+++ app/design/frontend/Alfakher/hookahwholesalers/Magento_Theme/web/js/view/messages.js
@@ -1,35 +1,53 @@
-/**
- * Copyright © 2020 Magento. All rights reserved.
- * See COPYING.txt for license details.
- */
 define([
     'jquery',
     'uiComponent',
     'Magento_Customer/js/customer-data',
+    'underscore',
+    'escaper',
     'jquery/jquery-storageapi'
-], function ($, Component, customerData) {
+], function ($, Component, customerData, _, escaper) {
     'use strict';
 
     return Component.extend({
+        defaults: {
+            cookieMessages: [],
+            messages: [],
+            allowedTags: ['div', 'span', 'b', 'strong', 'i', 'em', 'u', 'a']
+        },
+
+        /**
+         * Extends Component object by storage observable messages.
+         */
         initialize: function () {
             this._super();
-            this.cookieMessages = $.cookieStorage.get('mage-messages');
-            this.messages = customerData.get('messages').extend({disposableCustomerData: 'messages'});
-            $.cookieStorage.setConf({path: '/', expires: -1}).set('mage-messages', null);
-        },
-        beforeViewConstruction: function (selector) {
 
+            this.cookieMessages = _.unique($.cookieStorage.get('mage-messages'), 'text');
+            this.messages = customerData.get('messages').extend({
+                disposableCustomerData: 'messages'
+            });
+
+            // Force to clean obsolete messages
+            if (!_.isEmpty(this.messages().messages)) {
+                customerData.set('messages', {});
+            }
+
+            $.cookieStorage.set('mage-messages', '');
+            
         },
-        afterRender: function (type, selector) {
-            $(selector).addClass('animate-massage');
-            this.onClose(type, selector);
-        },
-        onClose : function(type, selector){
-            setTimeout(function closeNotificationMessage() {
-                $(selector).slideUp(1000, function () {
-                    $(this).removeClass('message-' + type + ' ' + type + ' animate-massage');
-                });
+
+        /**
+         * Prepare the given message to be rendered as HTML
+         *
+         * @param {String} message
+         * @return {String}
+         */
+        prepareMessageForHtml: function (message) {
+            $(".messages").show();
+            setTimeout(function() {
+                $(".messages").hide('blind', {}, 1000)
             }, 10000);
+            return escaper.escapeHtml(message, this.allowedTags);
         }
+        
     });
 });
