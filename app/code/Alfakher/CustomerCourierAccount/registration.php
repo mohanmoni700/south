8c1e5dac2f0af7ed910e7dfc41e64194e7f27f15
upendra <upendra.goswami@brainvire.com>
2022-04-28 11:10:09 +0530

    B2BHW-760 : custom shipping method for customer courier account info

---
+++ app/code/Alfakher/CustomerCourierAccount/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Alfakher_CustomerCourierAccount',
+    __DIR__
+);
