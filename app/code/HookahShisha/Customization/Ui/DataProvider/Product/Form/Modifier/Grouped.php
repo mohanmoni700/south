a3bf3912194d7f662f3ecad175eaaca571da29ae
karangar.meghnathi <karangar.meghnathi@brainvire.com>
2022-05-12 18:33:35 +0530

    B2BHW-802 : Removed unncessary function

--- app/code/HookahShisha/Customization/Ui/DataProvider/Product/Form/Modifier/Grouped.php
+++ app/code/HookahShisha/Customization/Ui/DataProvider/Product/Form/Modifier/Grouped.php
@@ -3,23 +3,18 @@ namespace HookahShisha\Customization\Ui\DataProvider\Product\Form\Modifier;
 
 use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Catalog\Api\Data\ProductLinkInterface;
-use Magento\Catalog\Model\Locator\LocatorInterface;
-use Magento\Catalog\Ui\DataProvider\Product\Form\Modifier\AbstractModifier;
-use Magento\Framework\Phrase;
-use Magento\Ui\Component\Modal;
-use Magento\Ui\Component\Form;
-use Magento\GroupedProduct\Model\Product\Type\Grouped as GroupedProductType;
-use Magento\Framework\UrlInterface;
-use Magento\Ui\Component\DynamicRows;
+use Magento\Catalog\Api\Data\ProductLinkInterfaceFactory;
 use Magento\Catalog\Api\ProductLinkRepositoryInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Catalog\Helper\Image as ImageHelper;
-use Magento\Eav\Api\AttributeSetRepositoryInterface;
+use Magento\Catalog\Model\Locator\LocatorInterface;
 use Magento\Catalog\Model\Product\Attribute\Source\Status;
+use Magento\Eav\Api\AttributeSetRepositoryInterface;
 use Magento\Framework\Locale\CurrencyInterface;
+use Magento\Framework\UrlInterface;
 use Magento\GroupedProduct\Model\Product\Link\CollectionProvider\Grouped as GroupedProducts;
-use Magento\Framework\App\ObjectManager;
-use Magento\Catalog\Api\Data\ProductLinkInterfaceFactory;
+use Magento\Ui\Component\DynamicRows;
+use Magento\Ui\Component\Form;
 
 /**
  * Data provider for Grouped products
@@ -27,12 +22,6 @@ use Magento\Catalog\Api\Data\ProductLinkInterfaceFactory;
  */
 class Grouped extends \Magento\GroupedProduct\Ui\DataProvider\Product\Form\Modifier\Grouped
 {
-    const GROUP_GROUPED = 'grouped';
-    const GROUP_CONTENT = 'content';
-    const DATA_SCOPE_GROUPED = 'grouped';
-    const SORT_ORDER = 20;
-    const LINK_TYPE = 'associated';
-
     /**
      * @var LocatorInterface
      */
@@ -112,7 +101,7 @@ class Grouped extends \Magento\GroupedProduct\Ui\DataProvider\Product\Form\Modif
      * @var Magento\CatalogInventory\Api\StockStateInterface
      */
     protected $stockState;
-    
+
     /**
      * @param LocatorInterface $locator
      * @param UrlInterface $urlBuilder
@@ -137,9 +126,9 @@ class Grouped extends \Magento\GroupedProduct\Ui\DataProvider\Product\Form\Modif
         Status $status,
         AttributeSetRepositoryInterface $attributeSetRepository,
         CurrencyInterface $localeCurrency,
-        \Magento\CatalogInventory\Api\StockStateInterface $stockState,
         array $uiComponentsConfig = [],
         GroupedProducts $groupedProducts = null,
+        \Magento\CatalogInventory\Api\StockStateInterface $stockState,
         \Magento\Catalog\Api\Data\ProductLinkInterfaceFactory $productLinkFactory = null
     ) {
         $this->locator = $locator;
@@ -151,39 +140,7 @@ class Grouped extends \Magento\GroupedProduct\Ui\DataProvider\Product\Form\Modif
         $this->attributeSetRepository = $attributeSetRepository;
         $this->localeCurrency = $localeCurrency;
         $this->stockState = $stockState;
-        $this->uiComponentsConfig = array_replace_recursive($this->uiComponentsConfig, $uiComponentsConfig);
-        $this->groupedProducts = $groupedProducts ?: ObjectManager::getInstance()->get(
-            \Magento\GroupedProduct\Model\Product\Link\CollectionProvider\Grouped::class
-        );
-        $this->productLinkFactory = $productLinkFactory ?: ObjectManager::getInstance()
-            ->get(\Magento\Catalog\Api\Data\ProductLinkInterfaceFactory::class);
-        parent::__construct($locator, $urlBuilder, $productLinkRepository, $productRepository, $imageHelper, $status, $attributeSetRepository, $localeCurrency);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function modifyData(array $data)
-    {
-        /** @var \Magento\Catalog\Model\Product $product */
-        $product = $this->locator->getProduct();
-        $modelId = $product->getId();
-        if ($modelId) {
-            $storeId = $this->locator->getStore()->getId();
-            $data[$product->getId()]['links'][self::LINK_TYPE] = [];
-            $linkedItems = $this->groupedProducts->getLinkedProducts($product);
-            usort($linkedItems, function ($a, $b) {
-                return $a->getPosition() <=> $b->getPosition();
-            });
-            $productLink = $this->productLinkFactory->create();
-            foreach ($linkedItems as $index => $linkItem) {
-                /** @var \Magento\Catalog\Api\Data\ProductInterface $linkedProduct */
-                $linkItem->setPosition($index);
-                $data[$modelId]['links'][self::LINK_TYPE][] = $this->fillData($linkItem, $productLink);
-            }
-            $data[$modelId][self::DATA_SOURCE_DEFAULT]['current_store_id'] = $storeId;
-        }
-        return $data;
+        parent::__construct($locator, $urlBuilder, $productLinkRepository, $productRepository, $imageHelper, $status, $attributeSetRepository, $localeCurrency, $uiComponentsConfig, $groupedProducts, $productLinkFactory);
     }
 
     /**
@@ -217,261 +174,6 @@ class Grouped extends \Magento\GroupedProduct\Ui\DataProvider\Product\Form\Modif
             'attribute_set' => $this->attributeSetRepository
                 ->get($linkedProduct->getAttributeSetId())
                 ->getAttributeSetName(),
-        ];
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function modifyMeta(array $meta)
-    {
-        if ($this->locator->getProduct()->getTypeId() === GroupedProductType::TYPE_CODE) {
-            $meta = array_replace_recursive(
-                $meta,
-                [
-                    static::GROUP_GROUPED => [
-                        'children' => $this->getChildren(),
-                        'arguments' => [
-                            'data' => [
-                                'config' => [
-                                    'label' => __('Grouped Products'),
-                                    'collapsible' => true,
-                                    'opened' => true,
-                                    'componentType' => Form\Fieldset::NAME,
-                                    'sortOrder' => $this->getNextGroupSortOrder(
-                                        $meta,
-                                        static::GROUP_CONTENT,
-                                        static::SORT_ORDER
-                                    ),
-                                ],
-                            ],
-                        ],
-                    ],
-                ]
-            );
-            $meta = $this->modifyQtyAndStockStatus($meta);
-        }
-        return $meta;
-    }
-
-    /**
-     * Disable Qty and Stock status fields
-     *
-     * @param array $meta
-     * @return array
-     */
-    protected function modifyQtyAndStockStatus(array $meta)
-    {
-        if ($groupCode = $this->getGroupCodeByField($meta, 'container_' . self::$codeQuantityAndStockStatus)) {
-            $parentChildren = &$meta[$groupCode]['children'];
-            if (!empty($parentChildren['container_' . self::$codeQuantityAndStockStatus])) {
-                $parentChildren['container_' . self::$codeQuantityAndStockStatus] = array_replace_recursive(
-                    $parentChildren['container_' . self::$codeQuantityAndStockStatus],
-                    [
-                        'children' => [
-                            self::$codeQuantityAndStockStatus => [
-                                'arguments' => [
-                                    'data' => [
-                                        'config' => ['disabled' => false],
-                                    ],
-                                ],
-                            ],
-                        ]
-                    ]
-                );
-            }
-        }
-        if ($groupCode = $this->getGroupCodeByField($meta, self::$codeQtyContainer)) {
-            $parentChildren = &$meta[$groupCode]['children'];
-            if (!empty($parentChildren[self::$codeQtyContainer])) {
-                $parentChildren[self::$codeQtyContainer] = array_replace_recursive(
-                    $parentChildren[self::$codeQtyContainer],
-                    [
-                        'children' => [
-                            self::$codeQty => [
-                                'arguments' => [
-                                    'data' => [
-                                        'config' => ['disabled' => true],
-                                    ],
-                                ],
-                            ],
-                        ],
-                    ]
-                );
-            }
-        }
-        return $meta;
-    }
-
-    /**
-     * Retrieve child meta configuration
-     *
-     * @return array
-     */
-    protected function getChildren()
-    {
-        return [
-            $this->uiComponentsConfig['button_set'] => $this->getButtonSet(),
-            $this->uiComponentsConfig['modal'] => $this->getModal(),
-            self::LINK_TYPE => $this->getGrid(),
-        ];
-    }
-
-    /**
-     * Returns Modal configuration
-     *
-     * @return array
-     */
-    protected function getModal()
-    {
-        return [
-            'arguments' => [
-                'data' => [
-                    'config' => [
-                        'componentType' => Modal::NAME,
-                        'dataScope' => '',
-                        'provider' =>
-                            $this->uiComponentsConfig['form']
-                            . '.'
-                            . $this->uiComponentsConfig['form']
-                            . '_data_source',
-                        'options' => [
-                            'title' => __('Add Products to Group'),
-                            'buttons' => [
-                                [
-                                    'text' => __('Cancel'),
-                                    'actions' => ['closeModal']
-                                ],
-                                [
-                                    'text' => __('Add Selected Products'),
-                                    'class' => 'action-primary',
-                                    'actions' => [
-                                        [
-                                            'targetName' => 'index = ' . $this->uiComponentsConfig['listing'],
-                                            'actionName' => 'save'
-                                        ],
-                                        'closeModal'
-                                    ],
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-            ],
-            'children' => [$this->uiComponentsConfig['listing'] => $this->getListing()],
-        ];
-    }
-
-    /**
-     * Returns Listing configuration
-     *
-     * @return array
-     */
-    protected function getListing()
-    {
-        return [
-            'arguments' => [
-                'data' => [
-                    'config' => [
-                        'autoRender' => false,
-                        'componentType' => 'insertListing',
-                        'dataScope' => $this->uiComponentsConfig['listing'],
-                        'externalProvider' =>
-                            $this->uiComponentsConfig['listing']
-                            . '.'
-                            . $this->uiComponentsConfig['listing']
-                            . '_data_source',
-                        'selectionsProvider' =>
-                            $this->uiComponentsConfig['listing']
-                            . '.'
-                            . $this->uiComponentsConfig['listing']
-                            . '.product_columns.ids',
-                        'ns' => $this->uiComponentsConfig['listing'],
-                        'render_url' => $this->urlBuilder->getUrl('mui/index/render'),
-                        'realTimeLink' => true,
-                        'provider' =>
-                            $this->uiComponentsConfig['form']
-                            . '.'
-                            . $this->uiComponentsConfig['form']
-                            . '_data_source',
-                        'dataLinks' => ['imports' => false, 'exports' => true],
-                        'behaviourType' => 'simple',
-                        'externalFilterMode' => true,
-                        'imports' => [
-                            'storeId' => '${ $.provider }:data.product.current_store_id',
-                            '__disableTmpl' => ['storeId' => false],
-                        ],
-                        'exports' => [
-                            'storeId' => '${ $.externalProvider }:params.current_store_id',
-                            '__disableTmpl' => ['storeId' => false],
-                        ],
-                    ],
-                ],
-            ],
-        ];
-    }
-
-    /**
-     * Returns Buttons Set configuration
-     *
-     * @return array
-     */
-    protected function getButtonSet()
-    {
-        return [
-            'arguments' => [
-                'data' => [
-                    'config' => [
-                        'formElement' => 'container',
-                        'componentType' => 'container',
-                        'label' => false,
-                        'content' => __(
-                            'A grouped product is made up of multiple, standalone products that are presented '
-                            . 'as a group. You can offer variations of a single product, or group them by season or '
-                            . 'theme to create a coordinated set. Each product can be purchased separately, '
-                            . 'or as part of the group.'
-                        ),
-                        'template' => 'ui/form/components/complex',
-                    ],
-                ],
-            ],
-            'children' => [
-                'grouped_products_button' => [
-                    'arguments' => [
-                        'data' => [
-                            'config' => [
-                                'formElement' => 'container',
-                                'componentType' => 'container',
-                                'component' => 'Magento_Ui/js/form/components/button',
-                                'actions' => [
-                                    [
-                                        'targetName' => $this->uiComponentsConfig['form'] .
-                                            '.' . $this->uiComponentsConfig['form']
-                                            . '.'
-                                            . static::GROUP_GROUPED
-                                            . '.'
-                                            . $this->uiComponentsConfig['modal'],
-                                        'actionName' => 'openModal',
-                                    ],
-                                    [
-                                        'targetName' => $this->uiComponentsConfig['form'] .
-                                            '.' . $this->uiComponentsConfig['form']
-                                            . '.'
-                                            . static::GROUP_GROUPED
-                                            . '.'
-                                            . $this->uiComponentsConfig['modal']
-                                            . '.'
-                                            . $this->uiComponentsConfig['listing'],
-                                        'actionName' => 'render',
-                                    ],
-                                ],
-                                'title' => __('Add Products to Group'),
-                                'provider' => null,
-                            ],
-                        ],
-                    ],
-                ],
-            ],
         ];
     }
 
@@ -518,31 +220,6 @@ class Grouped extends \Magento\GroupedProduct\Ui\DataProvider\Product\Form\Modif
                 ],
             ],
             'children' => $this->getRows(),
-        ];
-    }
-
-    /**
-     * Returns Dynamic rows records configuration
-     *
-     * @return array
-     */
-    protected function getRows()
-    {
-        return [
-            'record' => [
-                'arguments' => [
-                    'data' => [
-                        'config' => [
-                            'componentType' => 'container',
-                            'isTemplate' => true,
-                            'is_collection' => true,
-                            'component' => 'Magento_Ui/js/dynamic-rows/record',
-                            'dataScope' => '',
-                        ],
-                    ],
-                ],
-                'children' => $this->fillMeta(),
-            ],
         ];
     }
 
@@ -640,36 +317,6 @@ class Grouped extends \Magento\GroupedProduct\Ui\DataProvider\Product\Form\Modif
                             'sortOrder' => 120,
                             'visible' => false,
                         ],
-                    ],
-                ],
-            ],
-        ];
-    }
-
-    /**
-     * Returns text column configuration for the dynamic grid
-     *
-     * @param string $dataScope
-     * @param bool $fit
-     * @param Phrase $label
-     * @param int $sortOrder
-     * @return array
-     */
-    protected function getTextColumn($dataScope, $fit, Phrase $label, $sortOrder)
-    {
-        return [
-            'arguments' => [
-                'data' => [
-                    'config' => [
-                        'componentType' => Form\Field::NAME,
-                        'formElement' => Form\Element\Input::NAME,
-                        'elementTmpl' => 'ui/dynamic-rows/cells/text',
-                        'dataType' => Form\Element\DataType\Text::NAME,
-                        'dataScope' => $dataScope,
-                        'fit' => $fit,
-                        'label' => $label,
-                        'sortOrder' => $sortOrder,
-                        'labelVisible' => false,
                     ],
                 ],
             ],
