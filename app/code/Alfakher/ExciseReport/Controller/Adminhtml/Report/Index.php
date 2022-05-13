f4da1f1c3ab7f0058b320c3a687d8e274508ae72
bhavya.sheth <bhavya.sheth@brainvire.com>
2022-05-13 18:14:58 +0530

    B2BHW-856: Order extraction module name change

---
+++ app/code/Alfakher/ExciseReport/Controller/Adminhtml/Report/Index.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Alfakher\ExciseReport\Controller\Adminhtml\Report;
+
+class Index extends \Magento\Reports\Controller\Adminhtml\Report\Sales {
+	public function execute() {
+
+		$this->_initAction()->_setActiveMenu(
+			'Alfakher_ExciseReport::report_excisetax_order'
+		)->_addBreadcrumb(
+			__('Excise Order Report'),
+			__('Excise Order Report')
+		);
+		$this->_view->getPage()->getConfig()->getTitle()->prepend(__('Excise Order Report'));
+
+		$this->_view->renderLayout();
+	}
+}
