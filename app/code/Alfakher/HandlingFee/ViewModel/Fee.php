4d5704f96b3a2f098c2e3da1b7d8a3ddfd18c9ca
upendra <upendra.goswami@brainvire.com>
2022-05-19 15:37:04 +0530

    B2BHW-750 : resolving coding standard issue

--- app/code/Alfakher/HandlingFee/ViewModel/Fee.php
+++ app/code/Alfakher/HandlingFee/ViewModel/Fee.php
@@ -2,68 +2,68 @@
 
 namespace Alfakher\HandlingFee\ViewModel;
 
-/**
- * View model class
- *
- * @author af_bv_op
- */
-class Fee implements \Magento\Framework\View\Element\Block\ArgumentInterface {
+class Fee implements \Magento\Framework\View\Element\Block\ArgumentInterface
+{
 
-	const MODULE_ENABLE = "hookahshisha/handling_fee_group/handling_fee_enable";
-	const HANDLING_FEE_TYPE = "hookahshisha/handling_fee_group/handling_fee_type";
-	const HANDLING_FEE = "hookahshisha/handling_fee_group/handling_fee";
-	const SUBTOTAL_FEE = "hookahshisha/af_discount_group/subtotal_enable";
-	const SHIPPING_FEE = "hookahshisha/af_discount_group/shipping_enable";
-	const ZERO_OUT = "hookahshisha/af_discount_group/zero_out_enable";
+    const MODULE_ENABLE = "hookahshisha/handling_fee_group/handling_fee_enable";
+    const HANDLING_FEE_TYPE = "hookahshisha/handling_fee_group/handling_fee_type";
+    const HANDLING_FEE = "hookahshisha/handling_fee_group/handling_fee";
+    const SUBTOTAL_FEE = "hookahshisha/af_discount_group/subtotal_enable";
+    const SHIPPING_FEE = "hookahshisha/af_discount_group/shipping_enable";
+    const ZERO_OUT = "hookahshisha/af_discount_group/zero_out_enable";
 
-	/**
-	 * Constructor
-	 *
-	 * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
-	 */
-	public function __construct(
-		\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
-	) {
-		$this->scopeConfig = $scopeConfig;
-	}
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+    ) {
+        $this->scopeConfig = $scopeConfig;
+    }
 
-	/**
-	 * Check if module is enable
-	 *
-	 * @param int $websiteId
-	 */
-	public function isModuleEnabled($websiteId) {
-		$storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_WEBSITE;
-		return $this->scopeConfig->getValue(self::MODULE_ENABLE, $storeScope, $websiteId);
-	}
+    /**
+     * Check if module is enable
+     *
+     * @param int $websiteId
+     */
+    public function isModuleEnabled($websiteId)
+    {
+        $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_WEBSITE;
+        return $this->scopeConfig->getValue(self::MODULE_ENABLE, $storeScope, $websiteId);
+    }
 
-	/**
-	 * Check if subtotal edit is enable
-	 *
-	 * @param int $websiteId
-	 */
-	public function isSubtotalEditEnabled($websiteId) {
-		$storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_WEBSITE;
-		return $this->scopeConfig->getValue(self::SUBTOTAL_FEE, $storeScope, $websiteId);
-	}
+    /**
+     * Check if subtotal edit is enable
+     *
+     * @param int $websiteId
+     */
+    public function isSubtotalEditEnabled($websiteId)
+    {
+        $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_WEBSITE;
+        return $this->scopeConfig->getValue(self::SUBTOTAL_FEE, $storeScope, $websiteId);
+    }
 
-	/**
-	 * Check if shipping fee edit is enable
-	 *
-	 * @param int $websiteId
-	 */
-	public function isShippingFeeEditEnabled($websiteId) {
-		$storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_WEBSITE;
-		return $this->scopeConfig->getValue(self::SHIPPING_FEE, $storeScope, $websiteId);
-	}
+    /**
+     * Check if shipping fee edit is enable
+     *
+     * @param int $websiteId
+     */
+    public function isShippingFeeEditEnabled($websiteId)
+    {
+        $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_WEBSITE;
+        return $this->scopeConfig->getValue(self::SHIPPING_FEE, $storeScope, $websiteId);
+    }
 
-	/**
-	 * Check if zero out order is enable
-	 *
-	 * @param int $websiteId
-	 */
-	public function isZeroOutEnabled($websiteId) {
-		$storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_WEBSITE;
-		return $this->scopeConfig->getValue(self::ZERO_OUT, $storeScope, $websiteId);
-	}
+    /**
+     * Check if zero out order is enable
+     *
+     * @param int $websiteId
+     */
+    public function isZeroOutEnabled($websiteId)
+    {
+        $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_WEBSITE;
+        return $this->scopeConfig->getValue(self::ZERO_OUT, $storeScope, $websiteId);
+    }
 }
