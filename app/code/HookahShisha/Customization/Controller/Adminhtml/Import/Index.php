bca09d4e273aca7cedf653ca6598a0137b322dc7
Meet Patel <mitkumar.patel@brainvire.com>
2022-06-16 14:58:05 +0530

    B2BHW-875 : Email validation on account registration/creation

---
+++ app/code/HookahShisha/Customization/Controller/Adminhtml/Import/Index.php
@@ -0,0 +1,33 @@
+<?php
+namespace HookahShisha\Customization\Controller\Adminhtml\Import;
+
+class Index
+{
+
+    /**
+     * @var \Magento\Framework\Message\ManagerInterface
+     */
+    protected $messageManager;
+
+    /**
+     * Initialize dependencies
+     *
+     * @param \Magento\Framework\Message\ManagerInterface $messageManager
+     */
+    public function __construct(
+        \Magento\Framework\Message\ManagerInterface $messageManager
+    ) {
+        $this->messageManager = $messageManager;
+    }
+
+    /**
+     * Index action
+     *
+     * @param string $subject
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    public function beforeExecute(\Magento\ImportExport\Controller\Adminhtml\Import\Index $subject)
+    {
+        return $this->messageManager->addWarning(__("Please see to it that the email addresses are validated or they may cause the customer grid to break!"));
+    }
+}
