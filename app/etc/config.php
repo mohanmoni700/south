5103c3ae55ec917289f789433fa5294a41d45790
bhavya.sheth <bhavya.sheth@brainvire.com>
2022-07-04 13:07:28 +0530

    B2BHW-899:config.php

--- app/etc/config.php
+++ app/etc/config.php
@@ -719,6 +719,7 @@ return [
         'Alfakher_FinanceVerified' => 1,
         'MageWorx_OrdersBase' => 1,
         'MageWorx_OrderEditor' => 1,
+        'Alfakher_KlaviyoCustomCatalog' => 1,
         'Alfakher_MyDocument' => 1,
         'Alfakher_NotPaidInvoice' => 1,
         'Alfakher_OrderPdf' => 1,
