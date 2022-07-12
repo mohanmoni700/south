783b9814a2c62c209ce44e7180bcd2896a69e8df
yasin.vhora <yasin.vhora@brainvire.com>
2022-07-12 16:32:09 +0530

    B2BHW-958 : Add coment box on the FE during CO changes

--- app/code/Alfakher/OrderComment/registration.php
+++ app/code/Alfakher/OrderComment/registration.php
@@ -1,7 +1,5 @@
 <?php
 
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Alfakher_OrderComment',
-    __DIR__
-);
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Alfakher_OrderComment', __DIR__);
