1e429cc1bb1de89f903308e770769136be067159
sneha parmar <sneha.parmar@brainvire.com>
2022-08-03 16:17:57 +0530

    feature/B2BHW-1045 : Move event file to graphql area, Add configurations

--- app/code/HookahShisha/Customization/Observer/DisableCheckmo.php
+++ app/code/HookahShisha/Customization/Observer/DisableCheckmo.php
@@ -2,8 +2,10 @@
 
 namespace HookahShisha\Customization\Observer;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\App\State as state;
 use Magento\Framework\Event\Observer;
+use Magento\Store\Model\ScopeInterface;
 
 class DisableCheckmo implements \Magento\Framework\Event\ObserverInterface
 {
@@ -13,12 +15,20 @@ class DisableCheckmo implements \Magento\Framework\Event\ObserverInterface
     protected $state;
 
     /**
+     * @var ScopeInterface
+     */
+    protected $scopeConfig;
+
+    /**
      * @param State $state
+     * @param ScopeConfigInterface $scopeConfig
      */
     public function __construct(
-        State $state
+        State $state,
+        ScopeConfigInterface $scopeConfig
     ) {
         $this->state = $state;
+        $this->scopeConfig = $scopeConfig;
     }
 
     /**
@@ -28,12 +38,23 @@ class DisableCheckmo implements \Magento\Framework\Event\ObserverInterface
      */
     public function execute(Observer $observer)
     {
+        $isActive = $this->getStoreConfig('graphql_payment/payment_details/disable_payment_garphql');
+        $methods = $this->getStoreConfig('graphql_payment/payment_details/payment_needs_disable');
+        $paymentMethods = explode(',', $methods);
         $paymentCode = $observer->getEvent()->getMethodInstance()->getCode();
         $checkResult = $observer->getEvent()->getResult();
-        if ($paymentCode == 'checkmo') {
-            if ($this->state->getAreaCode() == 'graphql') {
-                $checkResult->setData('is_available', false);
-            }
+        if ($isActive == 1 && in_array($paymentCode, $paymentMethods)) {
+            $checkResult->setData('is_available', false);
         }
+    }
+
+    /**
+     * Method For get Configuration Values
+     *
+     * @param string $path
+     */
+    public function getStoreConfig($path)
+    {
+        return $this->scopeConfig->getValue($path, ScopeInterface::SCOPE_STORE);
     }
 }
