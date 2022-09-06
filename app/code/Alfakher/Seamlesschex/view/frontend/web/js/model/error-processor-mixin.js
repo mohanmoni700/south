d04c9679ef30a81722f7ad15712802b98bac9f8c
keval.patel <keval.patel@brainvire.com>
2022-09-06 15:23:24 +0530

    B2BHW-1049: Adding the dynamic message

--- app/code/Alfakher/Seamlesschex/view/frontend/web/js/model/error-processor-mixin.js
+++ app/code/Alfakher/Seamlesschex/view/frontend/web/js/model/error-processor-mixin.js
@@ -1,31 +1,20 @@
 define([
-    'mage/utils/wrapper'
-], function (wrapper) {
+    'mage/utils/wrapper',
+    'Magento_Ui/js/modal/alert',
+    'jquery'
+], function (wrapper, alert, $) {
     'use strict';
 
     return function (errorProcessor) {
         errorProcessor.process = wrapper.wrapSuper(errorProcessor.process, function (response, messageContainer) {
-           
             var error = JSON.parse(response.responseText);
-            if(error.message == "Your ACH payment request has been decline, please try with another ACH details or choose another payment method."){
-                showAlert();
-            }
+            alert({
+                title: $.mage.__('Unable to place order'),
+                content: $.mage.__(error.message)
+            });
             this._super(response, messageContainer);
         });
 
         return errorProcessor;
     };
 });
-function showAlert(){
-    require([
-    'Magento_Ui/js/modal/alert',
-    'jquery'
-    ], function(alert, $) {
-
-    alert({
-        title: $.mage.__('Unable to place order'),
-        content: $.mage.__('Your ACH payment request has been decline, please try with another ACH details or choose another payment method.')
-    });
-
-});
-}
