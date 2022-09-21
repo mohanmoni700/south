ffb8b180d24341ef5b9fbdc8b846704ed92835e2
Riddhi Rathod <riddhi.rathod@brainvire.com>
2022-09-21 15:17:22 +0530

    feature/ALFA-1551 : Out of stock products: Move OOS product to the bottom on PLP

---
+++ app/code/HookahShisha/OutOfStockGraphQl/registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @category  HookahShisha
+ * @package   HookahShisha_OutOfStockGraphQl
+ */
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'HookahShisha_OutOfStockGraphQl',
+    __DIR__
+);
