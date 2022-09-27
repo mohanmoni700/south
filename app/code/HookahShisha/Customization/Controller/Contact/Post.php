027eb65d48919a4940b7256d535f273260b2caa6
keval.patel <keval.patel@brainvire.com>
2022-09-27 12:29:36 +0530

    updated with coding standard

--- app/code/HookahShisha/Customization/Controller/Contact/Post.php
+++ app/code/HookahShisha/Customization/Controller/Contact/Post.php
@@ -10,70 +10,74 @@ use Magento\Framework\App\Request\DataPersistorInterface;
 use Magento\Framework\Exception\LocalizedException;
 use Psr\Log\LoggerInterface;
 
-class Post extends \Magento\Contact\Controller\Index\Post {
-	/**
-	 * @var DataPersistorInterface
-	 */
-	private $dataPersistor;
+class Post extends \Magento\Contact\Controller\Index\Post
+{
+    /**
+     * @var DataPersistorInterface
+     */
+    private $dataPersistor;
 
-	/**
-	 * @var Context
-	 */
-	private $context;
+    /**
+     * @var Context
+     */
+    private $context;
 
-	/**
-	 * @var MailInterface
-	 */
-	private $mail;
+    /**
+     * @var MailInterface
+     */
+    private $mail;
 
-	/**
-	 * @var LoggerInterface
-	 */
-	private $logger;
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
 
-	/**
-	 * @param Context $context
-	 * @param ConfigInterface $contactsConfig
-	 * @param MailInterface $mail
-	 * @param DataPersistorInterface $dataPersistor
-	 * @param LoggerInterface $logger
-	 */
-	public function __construct(
-		Context $context,
-		ConfigInterface $contactsConfig,
-		MailInterface $mail,
-		DataPersistorInterface $dataPersistor,
-		LoggerInterface $logger = null
-	) {
-		parent::__construct($context, $contactsConfig, $mail, $dataPersistor, $logger);
-		$this->context = $context;
-		$this->mail = $mail;
-		$this->dataPersistor = $dataPersistor;
-		$this->logger = $logger ?: ObjectManager::getInstance()->get(LoggerInterface::class);
-	}
+    /**
+     * @param Context $context
+     * @param ConfigInterface $contactsConfig
+     * @param MailInterface $mail
+     * @param DataPersistorInterface $dataPersistor
+     * @param LoggerInterface $logger
+     */
+    public function __construct(
+        Context $context,
+        ConfigInterface $contactsConfig,
+        MailInterface $mail,
+        DataPersistorInterface $dataPersistor,
+        LoggerInterface $logger = null
+    ) {
+        parent::__construct($context, $contactsConfig, $mail, $dataPersistor, $logger);
+        $this->context = $context;
+        $this->mail = $mail;
+        $this->dataPersistor = $dataPersistor;
+        $this->logger = $logger ?: ObjectManager::getInstance()->get(LoggerInterface::class);
+    }
 
-	/**
-	 * @return array
-	 * @throws \Exception
-	 */
-	private function validatedParams() {
-		$request = $this->getRequest();
-		if (trim($request->getParam('first_name')) === '') {
-			throw new LocalizedException(__('Enter the First Name and try again.'));
-		}
-		if (trim($request->getParam('last_name')) === '') {
-			throw new LocalizedException(__('Enter the Last Name and try again.'));
-		}
-		if (trim($request->getParam('comment')) === '') {
-			throw new LocalizedException(__('Enter the comment and try again.'));
-		}
-		if (false === \strpos($request->getParam('email'), '@')) {
-			throw new LocalizedException(__('The email address is invalid. Verify the email address and try again.'));
-		}
-		if (trim($request->getParam('hideit')) !== '') {
-			throw new \Exception();
-		}
+    /**
+     * Validate function
+     *
+     * @return array
+     * @throws \Exception
+     */
+    private function validatedParams()
+    {
+        $request = $this->getRequest();
+        if (trim($request->getParam('first_name')) === '') {
+            throw new LocalizedException(__('Enter the First Name and try again.'));
+        }
+        if (trim($request->getParam('last_name')) === '') {
+            throw new LocalizedException(__('Enter the Last Name and try again.'));
+        }
+        if (trim($request->getParam('comment')) === '') {
+            throw new LocalizedException(__('Enter the comment and try again.'));
+        }
+        if (false === \strpos($request->getParam('email'), '@')) {
+            throw new LocalizedException(__('The email address is invalid. Verify the email address and try again.'));
+        }
+        if (trim($request->getParam('hideit')) !== '') {
+            throw new LocalizedException(__(''));
+        }
 
-		return $request->getParams();
-	}
+        return $request->getParams();
+    }
 }
--- app/code/HookahShisha/Customization/Helper/UserData.php
+++ app/code/HookahShisha/Customization/Helper/UserData.php
@@ -13,78 +13,81 @@ use Magento\Framework\App\Request\DataPersistorInterface;
  * @deprecated 100.2.0
  * @see \Magento\Contact\Model\ConfigInterface
  */
-class UserData extends \Magento\Contact\Helper\Data {
-	const XML_PATH_ENABLED = ConfigInterface::XML_PATH_ENABLED;
+class UserData extends \Magento\Contact\Helper\Data
+{
+    public const XML_PATH_ENABLED = ConfigInterface::XML_PATH_ENABLED;
 
-	/**
-	 * Customer session
-	 *
-	 * @var \Magento\Customer\Model\Session
-	 */
-	protected $_customerSession;
+    /**
+     * Customer
+     *
+     * @var \Magento\Customer\Model\Session
+     */
+    protected $_customerSession;
 
-	/**
-	 * @var \Magento\Customer\Helper\View
-	 */
-	protected $_customerViewHelper;
+    /**
+     * @var \Magento\Customer\Helper\View
+     */
+    protected $_customerViewHelper;
 
-	/**
-	 * @var DataPersistorInterface
-	 */
-	private $dataPersistor;
+    /**
+     * @var DataPersistorInterface
+     */
+    private $dataPersistor;
 
-	/**
-	 * @var array
-	 */
-	private $postData = null;
+    /**
+     * @var array
+     */
+    private $postData = null;
 
-	/**
-	 * @param \Magento\Framework\App\Helper\Context $context
-	 * @param \Magento\Customer\Model\Session $customerSession
-	 * @param CustomerViewHelper $customerViewHelper
-	 */
-	public function __construct(
-		\Magento\Framework\App\Helper\Context $context,
-		\Magento\Customer\Model\Session $customerSession,
-		CustomerViewHelper $customerViewHelper
-	) {
-		$this->_customerSession = $customerSession;
-		$this->_customerViewHelper = $customerViewHelper;
-		parent::__construct($context, $customerSession, $customerViewHelper);
-	}
+    /**
+     * @param \Magento\Framework\App\Helper\Context $context
+     * @param \Magento\Customer\Model\Session $customerSession
+     * @param CustomerViewHelper $customerViewHelper
+     */
+    public function __construct(
+        \Magento\Framework\App\Helper\Context $context,
+        \Magento\Customer\Model\Session $customerSession,
+        CustomerViewHelper $customerViewHelper
+    ) {
+        $this->_customerSession = $customerSession;
+        $this->_customerViewHelper = $customerViewHelper;
+        parent::__construct($context, $customerSession, $customerViewHelper);
+    }
 
-	/**
-	 * Get first name
-	 *
-	 * @return string
-	 */
-	public function getFirstName() {
-		if (!$this->_customerSession->isLoggedIn()) {
-			return '';
-		}
+    /**
+     * Get first name
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        if (!$this->_customerSession->isLoggedIn()) {
+            return '';
+        }
 
-		/**
-		 * @var \Magento\Customer\Api\Data\CustomerInterface $customer
-		 */
-		$customer = $this->_customerSession->getCustomerDataObject();
-		$firstname = $customer->getFirstname();
-		return $firstname;
-	}
+        /**
+         * @var \Magento\Customer\Api\Data\CustomerInterface $customer
+         */
+        $customer = $this->_customerSession->getCustomerDataObject();
+        $firstname = $customer->getFirstname();
+        return $firstname;
+    }
 
-	/**
-	 * Get last name
-	 *
-	 * @return string
-	 */
-	public function getLastName() {
-		if (!$this->_customerSession->isLoggedIn()) {
-			return '';
-		}
-		/**
-		 * @var \Magento\Customer\Api\Data\CustomerInterface $customer
-		 */
-		$customer = $this->_customerSession->getCustomerDataObject();
-		$lastname = $customer->getLastname();
-		return $lastname;
-	}
+    /**
+     * Get last name
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        if (!$this->_customerSession->isLoggedIn()) {
+            return '';
+        }
+        /**
+         * @var \Magento\Customer\Api\Data\CustomerInterface $customer
+         */
+        $customer = $this->_customerSession->getCustomerDataObject();
+        $lastname = $customer->getLastname();
+        return $lastname;
+    }
 }
