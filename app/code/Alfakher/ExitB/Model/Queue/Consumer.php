379e60ca0a3ec6a08df7e19f0dd9a63959620fdc
Meet Patel <mitkumar.patel@brainvire.com>
2022-10-13 18:26:58 +0530

    feature/HUB-87 : Message Queue

---
+++ app/code/Alfakher/ExitB/Model/Queue/Consumer.php
@@ -0,0 +1,71 @@
+<?php
+namespace Alfakher\ExitB\Model\Queue;
+
+use Magento\Framework\MessageQueue\ConsumerConfiguration;
+
+/**
+ * Exitb Order
+ */
+class Consumer extends ConsumerConfiguration 
+{
+    public const TOPIC_NAME = "exitb.massorder.sync";
+    /**
+     * @var \Magento\Sales\Api\OrderRepositoryInterface 
+     */
+    private $orderRepository;
+    /**
+     * @var \Alfakher\ExitB\Helper\Data
+     */
+    protected $helperData;
+    /**
+     * @var \Magento\Framework\Serialize\Serializer\Json
+     */
+    protected $json;
+
+    /**
+     * Check construct
+     *
+     * @param 
+     * @param \Magento\Sales\Api\OrderRepositoryInterface  $orderRepository
+     * @param \Alfakher\ExitB\Helper\Data                  $helperData
+     * @param \Magento\Framework\Serialize\Serializer\Json $json
+     */
+    public function __construct(
+        \Magento\Sales\Api\OrderRepositoryInterface $orderRepository,
+        \Alfakher\ExitB\Helper\Data $helperData,
+        \Magento\Framework\Serialize\Serializer\Json $json
+    ) {
+        $this->order = $orderRepository;
+        $this->helperData = $helperData;
+        $this->json = $json;
+    }
+
+    /**
+     * Consumer process start
+     * 
+     * @param mixed $request
+     * @return void
+     */
+    public function process($request) 
+    {
+        $writer = new \Zend_Log_Writer_Stream(BP . '/var/log/mysql_message_queue.log');
+        $logger = new \Zend_Log();
+        $logger->addWriter($writer);
+        $logger->info('text message');
+        $logger->info(print_r('Request'.$request, true));
+
+        try {
+            $data = $this->json->unserialize($request, true);
+
+            $order = $this->order->get($data['orderId']);
+            $websiteId = $order->getStore()->getWebsiteId();
+            $token_value = $this->helperData->tokenAuthentication($websiteId);
+            $result = $this->helperData->orderSync($data['orderId'], $token_value);
+            
+            $logger->info(print_r("order id".$data['orderId'], true));
+            $logger->info(print_r($result, true));
+        } catch (\Exception $e) {
+            $logger->info("Error update.product.attribute: " . $e->getMessage());
+        }
+    }
+}
---
+++ app/code/Alfakher/ExitB/Plugin/Order/SyncPlugin.php
@@ -0,0 +1,79 @@
+<?php
+namespace Alfakher\ExitB\Plugin\Order;
+
+/**
+ * ExitB order sync
+ */
+class SyncPlugin
+{
+    public const TOPIC_NAME = 'exitb.massorder.sync';
+    /**
+     * @var \Magento\Framework\App\RequestInterface
+     */
+    protected $request;
+    /**
+     * @var \Magento\Sales\Api\OrderRepositoryInterface 
+     */
+    private $orderRepository;
+    /**
+     * @var \Alfakher\ExitB\Helper\Data
+     */
+    protected $helperData;
+    /**
+     * @var \Magento\Framework\Serialize\Serializer\Json 
+     */
+    protected $json;
+    /**
+     * @var \Magento\Framework\MessageQueue\PublisherInterface
+     */
+    protected $publisher;
+    
+    /**
+     * Check construct
+     *
+     * @param \Magento\Framework\App\RequestInterface      $request
+     * @param \Magento\Sales\Api\OrderRepositoryInterface  $orderRepository
+     * @param \Alfakher\ExitB\Model\ResourceModel\ExitbOrder\collection $exitborderModel
+     * @param \Alfakher\ExitB\Helper\Data                  $helperData
+     * @param \Magento\Framework\Serialize\Serializer\Json $json
+     * @param \Magento\Framework\MessageQueue\PublisherInterface $publisher
+     */
+    public function __construct(
+        \Magento\Framework\App\RequestInterface $request,
+        \Magento\Sales\Api\OrderRepositoryInterface $orderRepository,
+        \Alfakher\ExitB\Model\ResourceModel\ExitbOrder\collection $exitborderModel,
+        \Alfakher\ExitB\Helper\Data $helperData,
+        \Magento\Framework\Serialize\Serializer\Json $json,
+        \Magento\Framework\MessageQueue\PublisherInterface $publisher
+    ){
+        $this->request = $request;
+        $this->order = $orderRepository;
+        $this->exitborderModel = $exitborderModel;
+        $this->helperData = $helperData;
+        $this->json = $json;
+        $this->publisher = $publisher;
+    }
+
+    public function afterExecute(\Alfakher\SalesApprove\Controller\Adminhtml\Order\Approve $subject, 
+        $result)
+    {
+        $data = (array) $this->request->getParams();
+        $orderId = [
+            "orderId" => $data['order_id'],
+        ];
+        $order = $this->order->get($orderId['orderId']);
+        $websiteId = $order->getStore()->getWebsiteId();
+        
+        $status = $this->exitborderModel->addFieldToFilter('order_id', $orderId['orderId']);
+        $collection = $status->getColumnValues('sync_status');
+        $status_collection = !empty($collection) ? $collection[0] : null;
+
+        if ($this->helperData->isModuleEnabled($websiteId) && $status_collection !== '1') {
+            $this->publisher->publish(
+                self::TOPIC_NAME,
+                $this->json->serialize($orderId)
+            );
+        }
+        return $result;
+    }
+}
