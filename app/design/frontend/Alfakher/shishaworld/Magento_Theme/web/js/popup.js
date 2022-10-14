eba8bd5cf9be5bd2a154d6224d35d3062b143404
keval.patel <keval.patel@brainvire.com>
2022-10-14 16:11:56 +0530

    added page load document.ready for popup

---
+++ app/design/frontend/Alfakher/shishaworld/Magento_Theme/web/js/popup.js
@@ -0,0 +1,32 @@
+define([ 'jquery',
+'Magento_Ui/js/modal/modal',
+'mage/cookies'], function($,modal) {
+
+	var options = {
+		type: 'popup',
+		responsive: true,
+		innerScroll: true,
+		title: ' ',
+		modalClass: 'custom-window-block',
+		buttons: [{
+			class: '',
+			click: function () {
+			this.closeModal();
+			$('#popup').html(" ");
+			}
+		}],
+
+		opened: function($Event) {
+		$(".modal-footer").hide();
+		}
+	};
+
+	document.ready(function(){ 
+	var popup = modal(options, $('#popup'));
+	if ($.cookie('popuplogintext') != 'open') {
+			$("#popup").modal('openModal');
+		$.cookie('popuplogintext', 'open', { path: '/' }); //Set the cookies
+		}
+	});
+}
+);
