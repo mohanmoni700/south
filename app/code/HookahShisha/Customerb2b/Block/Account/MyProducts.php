45cdd03d2c6fd4851f4685c982abb7b1cbb135ed
bhavya <bhavya.sheth@brainvire.com>
2022-11-02 12:18:09 +0530

    feature/HUB-142 : My Product Pricing section store code

--- app/code/HookahShisha/Customerb2b/Block/Account/MyProducts.php
+++ app/code/HookahShisha/Customerb2b/Block/Account/MyProducts.php
@@ -2,83 +2,98 @@
 
 namespace HookahShisha\Customerb2b\Block\Account;
 
-use \Magento\Catalog\Model\Product\Attribute\Source\Status;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\Block\Product\Context as ProductContext;
+use Magento\Catalog\Model\ProductFactory;
+use Magento\Catalog\Model\Product\Attribute\Source\Status;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\App\Http\Context as HttpContext;
+use Magento\Framework\Model\ResourceModel\Iterator;
+use Magento\Framework\Url\Helper\Data;
+use Magento\SharedCatalog\Api\ProductItemRepositoryInterface;
+use Magento\SharedCatalog\Model\ResourceModel\ProductItem\CollectionFactory;
+use Magento\Store\Model\StoreManagerInterface;
 
 class MyProducts extends \Magento\Catalog\Block\Product\AbstractProduct
 {
 
     /**
-     * @var \Magento\Framework\App\Http\Context
+     * @var HttpContext
      */
     protected $httpContext;
 
     /**
-     * @var \Magento\Customer\Api\CustomerRepositoryInterface
+     * @var CustomerRepositoryInterface
      */
     protected $_customerRepositoryInterface;
 
     /**
-     * @var \Magento\SharedCatalog\Api\ProductItemRepositoryInterface
+     * @var ProductItemRepositoryInterface
      */
     protected $productItemRepository;
 
     /**
-     * @var \Magento\Catalog\Api\ProductRepositoryInterface
+     * @var ProductRepositoryInterface
      */
     private $productRepository;
 
     /**
-     * @var \Magento\Framework\Url\Helper\Data
+     * @var Data
      */
     protected $urlHelper;
 
     /**
-     * @var \Magento\Framework\Model\ResourceModel\Iterator
+     * @var Iterator
      */
     protected $iterator;
 
     /**
-     * @var \Magento\SharedCatalog\Model\ResourceModel\ProductItem\CollectionFactory
+     * @var CollectionFactory
      */
     protected $sharedCatalogCollection;
 
     /**
      * Constructor
      *
-     * @param \Magento\Catalog\Block\Product\Context $context
-     * @param \Magento\Framework\App\Http\Context $httpContext
-     * @param \Magento\Customer\Api\CustomerRepositoryInterface $customerRepositoryInterface
-     * @param \Magento\Framework\Api\SearchCriteriaBuilder $searchCriteriaBuilder
-     * @param \Magento\SharedCatalog\Api\ProductItemRepositoryInterface $productItemRepository
-     * @param \Magento\Catalog\Api\ProductRepositoryInterface $productRepository
-     * @param \Magento\Framework\Url\Helper\Data $urlHelper
-     * @param \Magento\Framework\Model\ResourceModel\Iterator $iterator
-     * @param \Magento\SharedCatalog\Model\ResourceModel\ProductItem\CollectionFactory $sharedCatalogCollection
-     * @param \Magento\Catalog\Model\ProductFactory $productFactory
+     * @param ProductRepositoryInterface $productRepository
+     * @param ProductContext $context
+     * @param ProductFactory $productFactory
+     * @param CustomerRepositoryInterface $customerRepositoryInterface
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     * @param HttpContext $httpContext
+     * @param Iterator $iterator
+     * @param Data $urlHelper
+     * @param ProductItemRepositoryInterface $productItemRepository
+     * @param CollectionFactory $sharedCatalogCollection
+     * @param StoreManagerInterface $storeManager
      * @param array $data = []
      */
+
     public function __construct(
-        \Magento\Catalog\Block\Product\Context $context,
-        \Magento\Framework\App\Http\Context $httpContext,
-        \Magento\Customer\Api\CustomerRepositoryInterface $customerRepositoryInterface,
-        \Magento\Framework\Api\SearchCriteriaBuilder $searchCriteriaBuilder,
-        \Magento\SharedCatalog\Api\ProductItemRepositoryInterface $productItemRepository,
-        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
-        \Magento\Framework\Url\Helper\Data $urlHelper,
-        \Magento\Framework\Model\ResourceModel\Iterator $iterator,
-        \Magento\SharedCatalog\Model\ResourceModel\ProductItem\CollectionFactory $sharedCatalogCollection,
-        \Magento\Catalog\Model\ProductFactory $productFactory,
+        ProductRepositoryInterface $productRepository,
+        ProductContext $context,
+        ProductFactory $productFactory,
+        CustomerRepositoryInterface $customerRepositoryInterface,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        HttpContext $httpContext,
+        Iterator $iterator,
+        Data $urlHelper,
+        ProductItemRepositoryInterface $productItemRepository,
+        CollectionFactory $sharedCatalogCollection,
+        StoreManagerInterface $storeManager,
         array $data = []
     ) {
-        $this->httpContext = $httpContext;
+        $this->productRepository = $productRepository;
+        $this->productFactory = $productFactory;
         $this->_customerRepositoryInterface = $customerRepositoryInterface;
         $this->searchCriteriaBuilder = $searchCriteriaBuilder;
-        $this->productItemRepository = $productItemRepository;
-        $this->productRepository = $productRepository;
-        $this->urlHelper = $urlHelper;
+        $this->httpContext = $httpContext;
         $this->iterator = $iterator;
+        $this->urlHelper = $urlHelper;
+        $this->productItemRepository = $productItemRepository;
         $this->sharedCatalogCollection = $sharedCatalogCollection;
-        $this->productFactory = $productFactory;
+        $this->storeManager = $storeManager;
         parent::__construct($context, $data);
     }
 
@@ -119,7 +134,8 @@ class MyProducts extends \Magento\Catalog\Block\Product\AbstractProduct
         if ($customer = $this->getCurrentCustomer()) {
             $page = ($this->getRequest()->getParam('p')) ? $this->getRequest()->getParam('p') : 1;
             $pageSize = ($this->getRequest()->getParam('limit')) ? $this->getRequest()->getParam('limit') : 20;
-            $collection = $this->productFactory->create()->getCollection();
+            $collection = $this->productFactory->create()->getCollection()
+                ->addStoreFilter($this->storeManager->getStore());
 
             $joinAndConditions[] = "u.sku = e.sku";
             $joinAndConditions[] = "u.customer_group_id= " . $customer->getGroupId();
