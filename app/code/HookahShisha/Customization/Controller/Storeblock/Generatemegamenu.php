9c72a93f2ec6f3690919d6c57183f9c1428dea4e
jigar_karangiya <jigar.karangiya@brainvire.com>
2022-11-22 13:21:47 +0530

    HUB-107 : Page Builder Fixes for Megamenu CMS Blocks

--- app/code/HookahShisha/Customization/Controller/Storeblock/Generatemegamenu.php
+++ app/code/HookahShisha/Customization/Controller/Storeblock/Generatemegamenu.php
@@ -1,28 +1,66 @@
 <?php
-
+declare(strict_types=1);
 namespace HookahShisha\Customization\Controller\Storeblock;
 
-/**
- *
- */
+use Magento\Cms\Model\BlockFactory;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Controller\Result\JsonFactory;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Store\Model\StoreManagerInterface;
+
 class Generatemegamenu extends \Magento\Framework\App\Action\Action
 {
+    /**
+     * @var Context
+     */
+    protected $context;
 
+    /**
+     * @var StoreManagerInterface
+     */
+    protected $storeManager;
+
+    /**
+     * @var BlockFactory
+     */
+    protected $blockFactory;
+
+    /**
+     * @var $resultJsonFactory
+     */
+    protected $resultJsonFactory;
+
+    /**
+     * @var $resultPageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Constructor for Generatemegamenu
+     *
+     * @param Context $context
+     * @param StoreManagerInterface $storeManager
+     * @param BlockFactory $blockFactory
+     * @param JsonFactory $resultJsonFactory
+     * @param PageFactory $resultPageFactory
+     */
     public function __construct(
-        \Magento\Framework\App\Action\Context $context,
-        \Magento\Store\Model\StoreManagerInterface $storeManager,
-        \Magento\Cms\Model\BlockFactory $blockFactory,
-        \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory,
-        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+        Context $context,
+        StoreManagerInterface $storeManager,
+        BlockFactory $blockFactory,
+        JsonFactory $resultJsonFactory,
+        PageFactory $resultPageFactory
     ) {
         parent::__construct($context);
-
         $this->_storeManager = $storeManager;
         $this->_blockFactory = $blockFactory;
         $this->_resultJsonFactory = $resultJsonFactory;
         $this->_pageFactory = $resultPageFactory;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function execute()
     {
 
@@ -31,13 +69,18 @@ class Generatemegamenu extends \Magento\Framework\App\Action\Action
 
         try {
             $resultPage = $this->_pageFactory->create();
-            $content = $resultPage->getLayout()->createBlock('Smartwave\Megamenu\Block\Topmenu')->setTemplate('Smartwave_Megamenu::topmenu.phtml')->toHtml();
+            $content = $resultPage->getLayout()
+                ->createBlock(Smartwave\Megamenu\Block\Topmenu::class)
+                ->setTemplate('Smartwave_Megamenu::topmenu.phtml')->toHtml();
 
             $updateBlock = $this->_blockFactory->create();
             $updateBlock->setStoreId($this->_storeManager->getStore()->getId())->load('megamenu-desktop', 'identifier');
 
             if ($updateBlock->getId()) {
-                $updateBlock->setContent($content);
+                $updateBlock->setContent('
+                    <div data-content-type="html" data-appearance="default" data-element="main">' .
+                    htmlentities($content) .
+                    '</div>');
                 $updateBlock->save();
             } else {
                 $newCmsStaticBlock = [
