1a7cbfb2dc29692e17d7ccdaf3966036eca4c3d6
upendra <upendra.goswami@brainvire.com>
2022-12-16 17:23:04 +0530

    rectifying the code

--- app/code/HookahShisha/SubscribeGraphQl/Plugin/Magedelight/Subscribenow/Model/Service/OrderService.php
+++ app/code/HookahShisha/SubscribeGraphQl/Plugin/Magedelight/Subscribenow/Model/Service/OrderService.php
@@ -6,14 +6,21 @@ use Magedelight\Subscribenow\Model\Service\OrderService as OrderServiceSubject;
 
 class OrderService
 {
-	
-	public function afterSetOrderInfo(
+    /**
+     * After set order info plugin
+     *
+     * @param OrderServiceSubject $subject
+     * @param object $result
+     * @param object $order
+     * @param object $item
+     */
+    public function afterSetOrderInfo(
         OrderServiceSubject $subject,
         $result,
         $order,
         $item
     ) {
-		$result->getSubscriptionModel()->setShippingAddressInfo(json_encode($order->getShippingAddress()->getData()));
+        $result->getSubscriptionModel()->setShippingAddressInfo(json_encode($order->getShippingAddress()->getData()));
         $result->getSubscriptionModel()->setBillingAddressInfo(json_encode($order->getBillingAddress()->getData()));
         return $result;
     }
