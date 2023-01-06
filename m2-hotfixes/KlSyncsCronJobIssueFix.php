c07a7af382dd9cfc873fec59bec631108354e331
Himmat Lal. Paliwal <corra-368@Corra-368.local>
2023-01-06 18:01:42 +0530

    ALFA-1852: Trying to access array offset on value of type null

--- m2-hotfixes/KlSyncsCronJobIssueFix.php
+++ m2-hotfixes/KlSyncsCronJobIssueFix.php
@@ -1,17 +1,14 @@
 diff --git a/vendor/klaviyo/magento2-extension/Cron/KlSyncs.php b/vendor/klaviyo/magento2-extension/Cron/KlSyncs.php
-index cefd5642..cb231d37 100644
+index cefd5642..d046d2f2 100644
 --- a/vendor/klaviyo/magento2-extension/Cron/KlSyncs.php
 +++ b/vendor/klaviyo/magento2-extension/Cron/KlSyncs.php
-@@ -125,9 +125,13 @@ class KlSyncs
-             if (in_array($topic, $trackApiTopics) && !empty($rows)) {
+@@ -126,8 +126,11 @@ class KlSyncs
                  foreach($rows as $row) {
                      $decodedPayload = json_decode($row['payload'], true);
--
+ 
 -                    $eventTime = $decodedPayload['time'];
 -                    unset($decodedPayload['time']);
-+                    if (empty($decodedPayload)) {
-+                        continue;
-+                    }
++                    $eventTime = '';
 +                    if (isset($decodedPayload['time'])) {
 +                        $eventTime = $decodedPayload['time'];
 +                        unset($decodedPayload['time']);
