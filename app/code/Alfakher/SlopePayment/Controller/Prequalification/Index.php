f7240f47d1d926eacae6178844dd5bc4d8136f99
jigar_karangiya <jigar.karangiya@brainvire.com>
2023-01-31 13:18:32 +0530

    B2BHW-1194 : Implemented slope prequalification widget in customer account section

---
+++ app/code/Alfakher/SlopePayment/Controller/Prequalification/Index.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace Alfakher\SlopePayment\Controller\Prequalification;
+
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * Result Page
+     *
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Index constructor.
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Execute method
+     *
+     * @return PageFactory
+     */
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
---
+++ app/code/Alfakher/SlopePayment/view/frontend/web/js/view/customer/slope-prequalify.js
@@ -0,0 +1,31 @@
+define([
+    'jquery',
+    'mage/url'
+], function ($, url) {
+    'use strict';
+
+    return function () {
+        $('#slope-prequalify').on('click', function () {
+            var apiUrl = url.build("slope/customer/prequalify");
+            $.ajax({
+                url: apiUrl,
+                type: 'GET',
+                showLoader: true,
+                success: function (data) {
+                    console.log(data);
+                    window.initializeSlope({
+                        flow: 'pre_qualify',
+                        intentSecret: data.secret,
+                    });
+                    window.Slope.open();
+                },
+                error: function (xhr, status, error) {
+                    //console.error(error);
+                },
+                complete: function (xhr, status) {
+                    //console.log(status);
+                }
+            });
+        });
+    }
+});
