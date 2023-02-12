c0dc5157f34e3f890611db3d5e8ff764e7bf2233
Samir <s.radi@hookah.com>
2023-02-12 07:42:11 +0530

    OOKA-50: updated the spreedly configuration to store_view scope

--- app/code/Corra/Spreedly/Gateway/Config/Config.php
+++ app/code/Corra/Spreedly/Gateway/Config/Config.php
@@ -1,11 +1,13 @@
 <?php
 /**
  * @author  CORRA
+ * @note - Configurations updated to store scope for JIRA::OOKA-50 to share new keys
  */
+
 namespace Corra\Spreedly\Gateway\Config;
 
+use Exception;
 use Magento\Framework\App\Config\ScopeConfigInterface;
-use Magento\Framework\App\ObjectManager;
 use Magento\Framework\Exception\InputException;
 use Magento\Framework\Exception\NoSuchEntityException;
 use PayPal\Braintree\Model\StoreConfigResolver;
@@ -25,12 +27,10 @@ class Config extends SourceConfig
     private const KEY_PAYEEZY_GATEWAY_TOKEN = 'payeezy_gateway_token';
     private const KEY_TITLE = 'title';
     private const KEY_PAYMENT_ACTION = 'payment_action';
-    private const KEY_ALLOWSPECIFIC = 'allowspecific';
-    private const KEY_SPECIFICCOUNTRY = 'specificcountry';
     private const KEY_CCTYPES = 'cctypes';
-    private const KEY_SERVICE_URL = "service_url";
-    private const KEY_TEST_MODE = "test_mode";
-    private const KEY_TEST_GATEWAY_TOKEN = "test_gateway_token";
+    private const KEY_SERVICE_URL = 'service_url';
+    private const KEY_TEST_MODE = 'test_mode';
+    private const KEY_TEST_GATEWAY_TOKEN = 'test_gateway_token';
     private const KEY_AUTHORIZENET_GATEWAY_ACTIVE = 'authorizenet_gateway_active';
     private const KEY_AUTHORIZENET_GATEWAY_DISTRIBUTION = 'authorizenet_gateway_distribution';
     private const KEY_PAYEEZY_GATEWAY_ACTIVE = 'payeezy_gateway_active';
@@ -39,203 +39,270 @@ class Config extends SourceConfig
     private const KEY_IS_CRON_ENABLED_REMOVE_REDACTED_SAVEDCC = 'cron_enabled';
     /**
      * @ref https://alfakher.atlassian.net/browse/OOKA-50
-     * @configkey "payment/spreedly/*"
+     * @configkey "payment/spreedly/(*_active || *_json)"
      */
     private const KEY_GATEWAY_SPECIFIC_FIELDS_ACTIVE = 'gateway_specific_fields_active';
     private const KEY_GATEWAY_SPECIFIC_FIELDS = 'gateway_specific_fields_json';
     /**
-     * @var Json
-     */
-    private $serializer;
-    /**
      * @var Logger
      */
-    protected $customLogger;
+    protected Logger $customLogger;
+    /**
+     * @var Json|null
+     */
+    private ?Json $serializer;
     /**
      * @var StoreConfigResolver
      */
-    private $storeConfigResolver;
+    private StoreConfigResolver $storeConfigResolver;
 
     /**
-     * Type constructer
+     * Type constructor
      *
      * @param StoreConfigResolver $storeConfigResolver
      * @param ScopeConfigInterface $scopeConfig
      * @param Logger $customLogger
-     * @param string $methodCode
+     * @param string|null $methodCode
      * @param string $pathPattern
-     * @param Json $serializer
+     * @param Json|null $serializer
      */
     public function __construct(
         StoreConfigResolver  $storeConfigResolver,
         ScopeConfigInterface $scopeConfig,
-        Logger $customLogger,
-        $methodCode = null,
-        string $pathPattern = self::DEFAULT_PATH_PATTERN,
-        Json $serializer = null
+        Logger               $customLogger,
+        string               $methodCode = null,
+        string               $pathPattern = self::DEFAULT_PATH_PATTERN,
+        Json                 $serializer = null
     ) {
         parent::__construct($scopeConfig, $methodCode, $pathPattern);
         $this->customLogger = $customLogger;
         $this->storeConfigResolver = $storeConfigResolver;
-        $this->serializer = $serializer ?: ObjectManager::getInstance()
-            ->get(Json::class);
+        $this->serializer = $serializer;
     }
 
     /**
      * Get PaymentMethod Active Status
      *
      * @return bool
+     * @throws InputException
+     * @throws NoSuchEntityException
      */
-    public function isActive()
+    public function isActive(): bool
     {
-        return (bool)$this->getValue(self::KEY_ACTIVE);
+        return (bool)$this->getValue(
+            self::KEY_ACTIVE,
+            $this->storeConfigResolver->getStoreId()
+        );
     }
+
     /**
      * Get Spreedly Environment Key
      *
-     * @return string
+     * @return mixed|null
+     * @throws InputException
+     * @throws NoSuchEntityException
      */
     public function getEnvironmentKey()
     {
-        return $this->getValue(self::KEY_ENVIRONMENT);
+        return $this->getValue(
+            self::KEY_ENVIRONMENT,
+            $this->storeConfigResolver->getStoreId()
+        );
     }
+
     /**
      * Get Spreedly Environment Secret
      *
-     * @return string
+     * @return mixed|null
+     * @throws InputException
+     * @throws NoSuchEntityException
      */
     public function getEnvironmentSecretKey()
     {
-        return $this->getValue(self::KEY_ENVIRONMENT_ACCESS_SECRET);
+        return $this->getValue(
+            self::KEY_ENVIRONMENT_ACCESS_SECRET,
+            $this->storeConfigResolver->getStoreId()
+        );
     }
+
     /**
      * Get AuthorizeNet Gateway Token
      *
-     * @return string
+     * @return mixed|null
+     * @throws InputException
+     * @throws NoSuchEntityException
      */
     public function getAuthorizeNetGatewayToken()
     {
-        return $this->getValue(self::KEY_AUTHORIZENET_GATEWAY_TOKEN);
+        return $this->getValue(
+            self::KEY_AUTHORIZENET_GATEWAY_TOKEN,
+            $this->storeConfigResolver->getStoreId()
+        );
     }
+
     /**
      * Get Payeezy Gateway Token
      *
-     * @return string
+     * @return mixed|null
+     * @throws InputException
+     * @throws NoSuchEntityException
      */
     public function getPayeezyGatewayToken()
     {
-        return $this->getValue(self::KEY_PAYEEZY_GATEWAY_TOKEN);
+        return $this->getValue(
+            self::KEY_PAYEEZY_GATEWAY_TOKEN,
+            $this->storeConfigResolver->getStoreId()
+        );
     }
+
     /**
      * Get Payment Method Title
      *
-     * @return string
+     * @return mixed|null
+     * @throws InputException
+     * @throws NoSuchEntityException
      */
     public function getTitle()
     {
-        return $this->getValue(self::KEY_TITLE);
+        return $this->getValue(
+            self::KEY_TITLE,
+            $this->storeConfigResolver->getStoreId()
+        );
     }
+
     /**
      * Get Payment Action value (Pending /Processing)
      *
      * @return mixed|null
+     * @throws InputException
+     * @throws NoSuchEntityException
      */
     public function getPaymentAction()
     {
-        return $this->getValue(self::KEY_PAYMENT_ACTION);
+        return $this->getValue(
+            self::KEY_PAYMENT_ACTION,
+            $this->storeConfigResolver->getStoreId()
+        );
     }
-    /**
-     * Get Allow Specific country selection
-     *
-     * @return mixed|null
-     */
-    public function getAllowspecific()
-    {
-        return $this->getValue(self::KEY_ALLOWSPECIFIC);
-    }
-    /**
-     * Get Specific country selection
-     *
-     * @return mixed|null
-     */
-    public function getSpecificcountry()
-    {
-        return $this->getValue(self::KEY_SPECIFICCOUNTRY);
-    }
+
     /**
      * Get CreditCard Types
      *
      * @return mixed|null
+     * @throws InputException
+     * @throws NoSuchEntityException
      */
     public function getCcTypes()
     {
-        return $this->getValue(self::KEY_CCTYPES);
+        return $this->getValue(
+            self::KEY_CCTYPES,
+            $this->storeConfigResolver->getStoreId()
+        );
     }
+
     /**
      * Get API Endpoint Url
      *
      * @return mixed|null
+     * @throws InputException
+     * @throws NoSuchEntityException
      */
     public function getServiceUrl()
     {
-        return $this->getValue(self::KEY_SERVICE_URL);
+        return $this->getValue(
+            self::KEY_SERVICE_URL,
+            $this->storeConfigResolver->getStoreId()
+        );
     }
+
     /**
      * Get TestMode value from system config
      *
      * @return bool
+     * @throws InputException
+     * @throws NoSuchEntityException
      */
-    public function getTestMode()
+    public function getTestMode():bool
     {
-        return (bool)$this->getValue(self::KEY_TEST_MODE);
+        return (bool)$this->getValue(
+            self::KEY_TEST_MODE,
+            $this->storeConfigResolver->getStoreId()
+        );
     }
+
     /**
      * Get TestGatewayToken value from system config
      *
-     * @return string
+     * @return mixed|null
+     * @throws InputException
+     * @throws NoSuchEntityException
      */
     public function getTestGatewayToken()
     {
-        return $this->getValue(self::KEY_TEST_GATEWAY_TOKEN);
+        return $this->getValue(
+            self::KEY_TEST_GATEWAY_TOKEN,
+            $this->storeConfigResolver->getStoreId()
+        );
     }
 
     /**
      * Get Authorizenet Gateway Active or not
      *
      * @return mixed|null
+     * @throws InputException
+     * @throws NoSuchEntityException
      */
     public function getAuthorizenetGatewayActive()
     {
-        return $this->getValue(self::KEY_AUTHORIZENET_GATEWAY_ACTIVE);
+        return $this->getValue(
+            self::KEY_AUTHORIZENET_GATEWAY_ACTIVE,
+            $this->storeConfigResolver->getStoreId()
+        );
     }
 
     /**
      * Get Payeezy Gateway Active or not
      *
      * @return mixed|null
+     * @throws InputException
+     * @throws NoSuchEntityException
      */
     public function getPayeezyGatewayActive()
     {
-        return $this->getValue(self::KEY_PAYEEZY_GATEWAY_ACTIVE);
+        return $this->getValue(
+            self::KEY_PAYEEZY_GATEWAY_ACTIVE,
+            $this->storeConfigResolver->getStoreId()
+        );
     }
+
     /**
      * Get Authorizenet Gateway Distribution
      *
      * @return mixed|null
+     * @throws InputException
+     * @throws NoSuchEntityException
      */
     public function getAuthorizenetGatewayDistribution()
     {
-        return $this->getValue(self::KEY_AUTHORIZENET_GATEWAY_DISTRIBUTION);
+        return $this->getValue(
+            self::KEY_AUTHORIZENET_GATEWAY_DISTRIBUTION,
+            $this->storeConfigResolver->getStoreId()
+        );
     }
 
     /**
      * Get Payeezy Gateway Distribution
      *
      * @return mixed|null
+     * @throws InputException
+     * @throws NoSuchEntityException
      */
     public function getPayeezyGatewayDistribution()
     {
-        return $this->getValue(self::KEY_PAYEEZY_GATEWAY_DISTRIBUTION);
+        return $this->getValue(
+            self::KEY_PAYEEZY_GATEWAY_DISTRIBUTION,
+            $this->storeConfigResolver->getStoreId()
+        );
     }
 
     /**
@@ -279,7 +346,7 @@ class Config extends SourceConfig
      */
     public function getGatewaySpecificFieldsJsonData()
     {
-        /** @var $gatewayFieldsActive - GET 'payment/spreedly/gateway_specific_fields_active' **/
+        /** @var $gatewayFieldsActive - GET 'payment/spreedly/gateway_specific_fields_active' * */
         $gatewayFieldsActive = $this->getValue(
             self::KEY_GATEWAY_SPECIFIC_FIELDS_ACTIVE,
             $this->storeConfigResolver->getStoreId()
@@ -294,7 +361,7 @@ class Config extends SourceConfig
              * Get 'gateway_specific_fields_json' data from admin configurations
              * If disabled @returns false
              */
-            $gatewayFieldsJsonData =$this->getValue(
+            $gatewayFieldsJsonData = $this->getValue(
                 self::KEY_GATEWAY_SPECIFIC_FIELDS,
                 $this->storeConfigResolver->getStoreId()
             );
@@ -302,9 +369,9 @@ class Config extends SourceConfig
              * Checking if correct JSON was updated in admin configurations
              */
             try {
-                /** @returns array **/
+                /** @returns array * */
                 return $this->serializer->unserialize($gatewayFieldsJsonData);
-            } catch (\Exception $e) {
+            } catch (Exception $e) {
                 $this->customLogger->debug(
                     (array)'Warning: JSON error on spreedly configuration for gateway_specific_fields. '
                 );
