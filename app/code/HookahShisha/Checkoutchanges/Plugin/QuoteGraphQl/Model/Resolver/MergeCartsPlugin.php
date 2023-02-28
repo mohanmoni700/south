f5b0c5fd23c3915af7a5948212892c110c8e602d
Harsh Darji <harshg.darji@brainvire.com>
2023-02-28 18:03:36 +0530

    update the code for mergecart plugin

--- app/code/HookahShisha/Checkoutchanges/Plugin/QuoteGraphQl/Model/Resolver/MergeCartsPlugin.php
+++ app/code/HookahShisha/Checkoutchanges/Plugin/QuoteGraphQl/Model/Resolver/MergeCartsPlugin.php
@@ -4,20 +4,14 @@ declare (strict_types = 1);
 namespace HookahShisha\Checkoutchanges\Plugin\QuoteGraphQl\Model\Resolver;
 
 use Magento\Customer\Api\CustomerRepositoryInterface;
-use Magento\Framework\App\ObjectManager;
-use Magento\Framework\Exception\CouldNotSaveException;
 use Magento\Framework\GraphQl\Config\Element\Field;
-use Magento\Framework\GraphQl\Exception\GraphQlAuthorizationException;
 use Magento\Framework\GraphQl\Exception\GraphQlInputException;
 use Magento\Framework\GraphQl\Exception\GraphQlNoSuchEntityException;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\GraphQl\Model\Query\ContextInterface;
-use Magento\QuoteGraphQl\Model\Cart\GetCartForUser;
-use Magento\QuoteGraphQl\Model\Cart\MergeCarts\CartQuantityValidatorInterface;
 use Magento\QuoteGraphQl\Model\Resolver\MergeCarts as Subject;
 use Magento\Quote\Api\CartRepositoryInterface;
-use Magento\Quote\Model\Cart\CustomerCartResolver;
-use Magento\Quote\Model\QuoteIdToMaskedQuoteIdInterface;
+use Magento\Quote\Model\MaskedQuoteIdToQuoteIdInterface;
 use Psr\Log\LoggerInterface;
 
 /**
@@ -28,140 +22,80 @@ use Psr\Log\LoggerInterface;
 class MergeCartsPlugin
 {
     /**
-     * @var GetCartForUser
-     */
-    private $getCartForUser;
-
-    /**
      * @var CartRepositoryInterface
      */
     private $cartRepository;
 
     /**
-     * @var CustomerCartResolver
-     */
-    private $customerCartResolver;
-
-    /**
-     * @var QuoteIdToMaskedQuoteIdInterface
-     */
-    private $quoteIdToMaskedQuoteId;
-
-    /**
-     * @var CartQuantityValidatorInterface
-     */
-    private $cartQuantityValidator;
-
-    /**
      * [__construct]
      *
-     * @param GetCartForUser $getCartForUser
      * @param CartRepositoryInterface $cartRepository
-     * @param CustomerCartResolver|null $customerCartResolver
-     * @param QuoteIdToMaskedQuoteIdInterface|null $quoteIdToMaskedQuoteId
-     * @param CartQuantityValidatorInterface|null $cartQuantityValidator
      * @param LoggerInterface $logger
      * @param CustomerRepositoryInterface $customerRepository
+     * @param MaskedQuoteIdToQuoteIdInterface $maskedQuoteIdToQuoteId
      */
     public function __construct(
-        GetCartForUser $getCartForUser,
         CartRepositoryInterface $cartRepository,
-        CustomerCartResolver $customerCartResolver = null,
-        QuoteIdToMaskedQuoteIdInterface $quoteIdToMaskedQuoteId = null,
-        CartQuantityValidatorInterface $cartQuantityValidator = null,
         LoggerInterface $logger,
-        CustomerRepositoryInterface $customerRepository
+        CustomerRepositoryInterface $customerRepository,
+        MaskedQuoteIdToQuoteIdInterface $maskedQuoteIdToQuoteId
     ) {
-        $this->getCartForUser = $getCartForUser;
         $this->cartRepository = $cartRepository;
-        $this->customerCartResolver = $customerCartResolver
-        ?: ObjectManager::getInstance()->get(CustomerCartResolver::class);
-        $this->quoteIdToMaskedQuoteId = $quoteIdToMaskedQuoteId
-        ?: ObjectManager::getInstance()->get(QuoteIdToMaskedQuoteIdInterface::class);
-        $this->cartQuantityValidator = $cartQuantityValidator
-        ?: ObjectManager::getInstance()->get(CartQuantityValidatorInterface::class);
         $this->logger = $logger;
         $this->customerRepository = $customerRepository;
+        $this->maskedQuoteIdToQuoteId = $maskedQuoteIdToQuoteId;
     }
 
     /**
-     * [beforeResolve]
+     * [afterResolve]
      *
      * @param Subject $subject
+     * @param mixed $result
      * @param Field $field
      * @param mixed $context
      * @param ResolveInfo $info
-     * @param array|null $value
-     * @param array|null $args
-     * @return mixed
+     * @param array $value
+     * @param array $args
      * @throws GraphQlInputException
+     * @throws GraphQlNoSuchEntityException
+     *
+     * @return $result
      */
-    public function beforeResolve(
+    public function afterResolve(
         Subject $subject,
+        $result,
         Field $field,
         $context,
         ResolveInfo $info,
         array $value = null,
         array $args = null
     ) {
+
+        $currentUserId = $context->getUserId();
         if (empty($args['source_cart_id'])) {
             throw new GraphQlInputException(__(
                 'Required parameter "source_cart_id" is missing'
             ));
         }
-
-        /** @var ContextInterface $context */
-        if (false === $context->getExtensionAttributes()->getIsCustomer()) {
-            throw new GraphQlAuthorizationException(__(
-                'The current customer isn\'t authorized.'
-            ));
-        }
-        $currentUserId = $context->getUserId();
-
-        if (!isset($args['destination_cart_id'])) {
-            try {
-                $cart = $this->customerCartResolver->resolve($currentUserId);
-            } catch (CouldNotSaveException $exception) {
-                throw new GraphQlNoSuchEntityException(
-                    __('Could not create empty cart for customer'),
-                    $exception
-                );
-            }
-            $customerMaskedCartId = $this->quoteIdToMaskedQuoteId->execute(
-                (int) $cart->getId()
-            );
-        } else {
-            if (empty($args['destination_cart_id'])) {
-                throw new GraphQlInputException(__(
-                    'The parameter "destination_cart_id" cannot be empty'
-                ));
-            }
-        }
         $guestMaskedCartId = $args['source_cart_id'];
-        $customerMaskedCartId = $customerMaskedCartId ?? $args['destination_cart_id'];
-        $storeId = (int) $context->getExtensionAttributes()->getStore()->getId();
-        try {
-            $guestCart = $this->getCartForUser->execute(
-                $guestMaskedCartId,
-                null,
-                $storeId
-            );
-            $customerCart = $this->getCartForUser->execute(
-                $customerMaskedCartId,
-                $currentUserId,
-                $storeId
-            );
-            if ($this->cartQuantityValidator->validateFinalCartQuantities($customerCart, $guestCart)) {
-                $guestCart = $this->getCartForUser->execute(
-                    $guestMaskedCartId,
-                    null,
-                    $storeId
-                );
-            }
+        $cartId = $this->maskedQuoteIdToQuoteId->execute($guestMaskedCartId);
 
+        try {
+            /** @var Quote $cart */
+            $guestCart = $this->cartRepository->get($cartId);
+        } catch (NoSuchEntityException $e) {
+            throw new GraphQlNoSuchEntityException(
+                __('Could not find a cart with ID "%masked_cart_id"', ['masked_cart_id' => $cartId])
+            );
+        }
+
+        $storeId = (int)$context->getExtensionAttributes()->getStore()->getId();
+        $customerCart=$result['model'];
+
+        if ($customerCart != null) {
             if ($guestCart && $guestCart->getShippingAddress()) {
-                    $customer=$this->customerRepository->getById($currentUserId);
-                    $shippingAddressId = $customer->getDefaultShipping();
+                $customer=$this->customerRepository->getById($currentUserId);
+                $shippingAddressId = $customer->getDefaultShipping();
                 if ($shippingAddressId === null || $shippingAddressId === 0) {
                     $customerCart->getShippingAddress()
                         ->setFirstname($guestCart->getShippingAddress()->getFirstname());
@@ -185,14 +119,12 @@ class MergeCartsPlugin
                         ->setCounty($guestCart->getShippingAddress()->getCounty());
                 }
             }
-            try {
-                $this->cartRepository->save($customerCart);
-            } catch (\Exception $e) {
-                $this->logger->error($e->getMessage());
-                throw new GraphQlInputException(__($e->getMessage()));
-            }
+        }
+        try {
+            $this->cartRepository->save($customerCart);
         } catch (\Exception $e) {
             $this->logger->error($e->getMessage());
         }
+        return $result;
     }
 }
