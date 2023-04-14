74f72d252c53cbb5cd3ab8cb2c31321f7ebe37d4
Devendra Gupta <devendra.k@codilar.com>
2023-04-14 13:02:52 +0530

    SW-231 GraphQl Translation Issue

---
+++ app/code/Vrpayecommerce/Vrpayecommerce/Plugin/Controller/GraphQlPlugin.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author Codilar
+ */
+declare(strict_types=1);
+namespace Vrpayecommerce\Vrpayecommerce\Plugin\Controller;
+
+use Magento\Framework\App\Area;
+use Magento\Framework\App\AreaList;
+use Magento\Framework\App\State;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\GraphQl\Controller\GraphQl;
+
+class GraphQlPlugin
+{
+    /** @var AreaList $areaList */
+    private $areaList;
+
+    /** @var State $appState */
+    private $appState;
+
+    /**
+     * @param AreaList $areaList
+     * @param State $appState
+     */
+    public function __construct(
+        AreaList $areaList,
+        State    $appState
+    ) {
+        $this->areaList = $areaList;
+        $this->appState = $appState;
+    }
+
+    /**
+     * @param GraphQl $subject
+     * @throws LocalizedException
+     */
+    public function beforeDispatch(GraphQl $subject)
+    {
+        $area = $this->areaList->getArea($this->appState->getAreaCode());
+        $area->load(Area::PART_TRANSLATE);
+    }
+}
