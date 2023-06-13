aba84edaa784cde021c4e7dd03ec82092c06b09c
liya <Liya.George@hookah.com>
2023-06-13 16:24:01 +0530

    b2bhw-1428-update

--- app/code/Alfakher/RequestQuote/Plugin/RequestQuote/Controller/Move/InCart.php
+++ app/code/Alfakher/RequestQuote/Plugin/RequestQuote/Controller/Move/InCart.php
@@ -2,45 +2,73 @@
 
 namespace Alfakher\RequestQuote\Plugin\RequestQuote\Controller\Move;
 
+use Amasty\RequestQuote\Api\QuoteRepositoryInterface as AmastyQuoteRepository;
 use Amasty\RequestQuote\Controller\Move\InCart as AmastyInCart;
-use Magento\Quote\Model\QuoteFactory;
+use Magento\Framework\Controller\Result\Redirect;
+use Magento\Quote\Api\CartRepositoryInterface as MagentoQuoteRepository;
 use Magento\Framework\App\RequestInterface;
+use Magento\Checkout\Model\Session as CheckoutSession;
+
 class InCart
 {
-    /** @var QuoteFactory */
-    protected QuoteFactory $quoteFactory;
-
     /**
      * @var RequestInterface
      */
     private RequestInterface $request;
+    /**
+     * @var CheckoutSession
+     */
+    private CheckoutSession $checkoutSession;
 
     /**
-     * @param QuoteFactory $quoteFactory
+     * @var AmastyQuoteRepository
+     */
+    private AmastyQuoteRepository $amastyQuoteRepository;
+    /**
+     * @var MagentoQuoteRepository
+     */
+    private MagentoQuoteRepository $magentoQuoteRepository;
+
+    /**
+     * @param CheckoutSession $checkoutSession
+     * @param AmastyQuoteRepository $amastyQuoteRepository
      * @param RequestInterface $request
+     * @param MagentoQuoteRepository $magentoQuoteRepository
      */
     public function __construct(
-        QuoteFactory $quoteFactory,
-        RequestInterface $request
+        CheckoutSession $checkoutSession,
+        AmastyQuoteRepository $amastyQuoteRepository,
+        RequestInterface $request,
+        MagentoQuoteRepository $magentoQuoteRepository
+
     ) {
-        $this->quoteFactory = $quoteFactory;
         $this->request = $request;
+        $this->amastyQuoteRepository = $amastyQuoteRepository;
+        $this->checkoutSession = $checkoutSession;
+        $this->magentoQuoteRepository = $magentoQuoteRepository;
     }
 
     /**
      * Amasty After move to cart Plugin
      *
      * @param AmastyInCart $subject
-     * @param mixed $result
-     * @return mixed
+     * @param Redirect $result
+     * @return Redirect
      */
-    public function afterExecute(AmastyInCart $subject, $result)
+    public function afterExecute(AmastyInCart $subject, \Magento\Framework\Controller\Result\Redirect $result)
     {
         $quoteId = (int) $this->request->getParam('quote_id');
-        if (!empty($quoteId)) {
-            $quote = $this->quoteFactory->create();
-            $quote->load($quoteId);
-            $quote->collectTotals()->save();
+        if ($quoteId) {
+            $currentQuote = $this->checkoutSession->getQuote();
+            $approvedQuote = $this->amastyQuoteRepository->get($quoteId);
+            if ($currentQuote->getSubtotal() == 0 || $currentQuote->getBaseSubtotal() == 0
+                || $currentQuote->getGrandTotal() == 0|| $approvedQuote->getBaseGrandTotal() == 0) {
+                $currentQuote->setSubtotal($approvedQuote->getSubtotal());
+                $currentQuote->setBaseGrandTotal($approvedQuote->getBaseGrandTotal());
+                $currentQuote->setGrandTotal($approvedQuote->getGrandTotal());
+                $currentQuote->setBaseSubtotal($approvedQuote->getBaseSubtotal());
+            }
+            $this->magentoQuoteRepository->save($currentQuote);
         }
         return $result;
     }
