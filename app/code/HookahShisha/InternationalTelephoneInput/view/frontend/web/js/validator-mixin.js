6a3d8332c25d3e25e2389520c2e9cb52cbbc7612
Devendra Gupta <devendra.k@codilar.com>
2023-06-13 09:59:30 +0530

    Updated code review changes

--- app/code/HookahShisha/InternationalTelephoneInput/view/frontend/web/js/validator-mixin.js
+++ app/code/HookahShisha/InternationalTelephoneInput/view/frontend/web/js/validator-mixin.js
@@ -7,9 +7,9 @@ define([
         validator.addRule(
             'custom-validate-telephone',
             function (value, params) {
-                var phoneno1 = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
-                var phoneno2 = /^\+?\d+$/;
-                var phoneno3 = /^\+\d{1,4}\s\d+$/;
+                let phoneno1 = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
+                let phoneno2 = /^\+?\d+$/;
+                let phoneno3 = /^\+\d{1,4}\s\d+$/;
                 if((value.match(phoneno1)) || (value.match(phoneno2)) || (value.match(phoneno3))){
                     return true;
                 }
