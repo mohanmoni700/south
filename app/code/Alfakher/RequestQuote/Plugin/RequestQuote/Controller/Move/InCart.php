f4a02b9cc32b0fbe393f47b1cab2a706dcd9ab6d
liya <Liya.George@hookah.com>
2023-06-14 17:00:19 +0530

    b2bhw-1428 - multiple product in quote subtotal value 0 issue fixed

--- app/code/Alfakher/RequestQuote/Plugin/RequestQuote/Controller/Move/InCart.php
+++ app/code/Alfakher/RequestQuote/Plugin/RequestQuote/Controller/Move/InCart.php
@@ -2,28 +2,18 @@
 
 namespace Alfakher\RequestQuote\Plugin\RequestQuote\Controller\Move;
 
-use Amasty\RequestQuote\Api\QuoteRepositoryInterface as AmastyQuoteRepository;
 use Amasty\RequestQuote\Controller\Move\InCart as AmastyInCart;
 use Magento\Framework\Controller\Result\Redirect;
 use Magento\Quote\Api\CartRepositoryInterface as MagentoQuoteRepository;
-use Magento\Framework\App\RequestInterface;
 use Magento\Checkout\Model\Session as CheckoutSession;
 
 class InCart
 {
     /**
-     * @var RequestInterface
-     */
-    private RequestInterface $request;
-    /**
      * @var CheckoutSession
      */
     private CheckoutSession $checkoutSession;
 
-    /**
-     * @var AmastyQuoteRepository
-     */
-    private AmastyQuoteRepository $amastyQuoteRepository;
     /**
      * @var MagentoQuoteRepository
      */
@@ -31,19 +21,13 @@ class InCart
 
     /**
      * @param CheckoutSession $checkoutSession
-     * @param AmastyQuoteRepository $amastyQuoteRepository
-     * @param RequestInterface $request
      * @param MagentoQuoteRepository $magentoQuoteRepository
      */
     public function __construct(
         CheckoutSession $checkoutSession,
-        AmastyQuoteRepository $amastyQuoteRepository,
-        RequestInterface $request,
         MagentoQuoteRepository $magentoQuoteRepository
 
     ) {
-        $this->request = $request;
-        $this->amastyQuoteRepository = $amastyQuoteRepository;
         $this->checkoutSession = $checkoutSession;
         $this->magentoQuoteRepository = $magentoQuoteRepository;
     }
@@ -57,19 +41,10 @@ class InCart
      */
     public function afterExecute(AmastyInCart $subject, \Magento\Framework\Controller\Result\Redirect $result)
     {
-        $quoteId = (int) $this->request->getParam('quote_id');
-        if ($quoteId) {
-            $currentQuote = $this->checkoutSession->getQuote();
-            $approvedQuote = $this->amastyQuoteRepository->get($quoteId);
-            if ($currentQuote->getSubtotal() == 0 || $currentQuote->getBaseSubtotal() == 0
-                || $currentQuote->getGrandTotal() == 0|| $approvedQuote->getBaseGrandTotal() == 0) {
-                $currentQuote->setSubtotal($approvedQuote->getSubtotal());
-                $currentQuote->setBaseGrandTotal($approvedQuote->getBaseGrandTotal());
-                $currentQuote->setGrandTotal($approvedQuote->getGrandTotal());
-                $currentQuote->setBaseSubtotal($approvedQuote->getBaseSubtotal());
-            }
-            $this->magentoQuoteRepository->save($currentQuote);
-        }
+
+        $qtID = $this->checkoutSession->getQuoteId();
+        $currentQuote = $this->magentoQuoteRepository->get($qtID);
+        $this->magentoQuoteRepository->save($currentQuote);
         return $result;
     }
 }
