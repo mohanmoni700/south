9deedb1085846b946fdd292a5230d0dfbd192aae
Pradyumna Bhat <pradyumna@codilar.com>
2023-07-04 16:07:08 +0530

    OOKA-300: Tabby rejection exceptions handling

--- app/code/Alfakher/Tabby/Model/TabbyCheckout.php
+++ app/code/Alfakher/Tabby/Model/TabbyCheckout.php
@@ -11,6 +11,22 @@ use Tabby\Checkout\Model\Method\Checkout;
 
 class TabbyCheckout extends Checkout
 {
+
+    /**
+     * Array to hold Tabby rejection reason code and message
+     *
+     * @var array|string[]
+     */
+    private array $rejectionReasons = [
+        'not_available' =>
+            'Sorry, Tabby is unable to approve this purchase. Please use an alternative payment method for your order.',
+        'order_amount_too_high' =>
+            'This purchase is above your current spending limit with Tabby,
+            try a smaller cart or use another payment method',
+        'order_amount_too_low' =>
+            'The purchase amount is below the minimum amount required to use Tabby, try adding more items or use another payment method'
+    ];
+
     protected $_codeTabby = 'installments';
 
     /**
@@ -58,11 +74,25 @@ class TabbyCheckout extends Checkout
                     throw new LocalizedException(__("Selected payment method not available."));
                 }
             } else {
-                throw new LocalizedException(__("Response not have status field or payment rejected"));
+                if (property_exists($result->configuration->products->installments, 'rejection_reason')
+                    && isset($this->rejectionReasons[$result->configuration->products->installments->rejection_reason])
+                ) {
+                    throw new LocalizedException(
+                        __($this->rejectionReasons[$result->configuration->products->installments->rejection_reason]),
+                        null,
+                        '100'
+                    );
+                } else {
+                    throw new LocalizedException(__("Response not have status field or payment rejected"));
+                }
             }
         } catch (\Exception $e) {
             $this->_ddlog->log("error", "createSession exception", $e, $requestData);
-            throw new LocalizedException(__("Something went wrong. Please try again later or contact support."));
+            if ($e->getCode() === 100) {
+                throw $e;
+            } else {
+                throw new LocalizedException(__("Something went wrong. Please try again later or contact support."));
+            }
         }
 
         return $redirectUrl;
