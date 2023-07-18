87cb55efeecec3e81e765833460829bd5dc635f9
liya <Liya.George@hookah.com>
2023-07-18 15:55:26 +0530

    B2BHW-1398-hide loyalty rewards text box if it has quoted products

--- app/design/frontend/Alfakher/hookahwholesalers/Yotpo_Loyalty/web/js/view/summary/custom.js
+++ app/design/frontend/Alfakher/hookahwholesalers/Yotpo_Loyalty/web/js/view/summary/custom.js
@@ -7,61 +7,40 @@ define(
     ],
     function (Component, quote, customerData, _) {
         'use strict';
-
-        var mixin = {
+        return Component.extend({
             defaults: {
                 template: 'Yotpo_Loyalty/summary/custom'
             },
-            loadJsCustomAfterKoRender: function() {
+            loadJsCustomAfterKoRender: function () {
+                var quoteItemData = {};
+                var isAmastyQuoteItem = false;
                 var guidId = window.valuesConfig;
                 var instanceId = window.swellInstanceId;
                 var url = 'https://cdn-widgetsrepository.yotpo.com/v1/loader/' + guidId;
                 var script = document.createElement('script');
-                script.src = url;
-                script.setAttribute('src_type', 'url');
-                document.head.appendChild(script);
+                script.src = url
+                script.setAttribute('src_type', 'url')
+                document.head.appendChild(script)
                 jQuery('.yotpo-widget-instance').attr('data-yotpo-instance-id', instanceId);
 
                 // Get current cart quote
-                var currentTotals = quote.getTotals();
-                if (currentTotals && currentTotals.items) {
-                    // Pass item to _isAmastyQuoteItem function
-                    var item = currentTotals.items[0]; // Assuming you want to pass the first item in the quote
-                    var isAmastyQuoteItem = this._isAmastyQuoteItem(item);
-                    console.log('Is Amasty Quote Item:', isAmastyQuoteItem);
-                }
-
-                if (isAmastyQuoteItem)
-                {
-                    jQuery('.parent').hide();
-                }
-            },
-
-            /**
-             * @param {Object} item
-             * @return {Boolean}
-             * @private
-             */
-            _isAmastyQuoteItem: function (item) {
-                // Implementation of _isAmastyQuoteItem() from the previous code snippet
-                var cartData = customerData.get('cart')();
-                var quoteItemData = {};
-                var isAmastyQuoteItem = false;
-
-                if (!_.isUndefined(cartData.items)) {
+                var quoteData = customerData.get('cart')();
+                if (!_.isUndefined(quoteData.items)) {
                     // Find the item in the cart data that matches the given item's item_id
-                    quoteItemData = _.find(cartData.items, function (itemData) {
-                        return itemData.item_id == item.item_id;
-                    });
-
-                    // If the quoteItemData is found, check if it has the 'is_amasty_quote_item' property
                     if (quoteItemData) {
-                        isAmastyQuoteItem = !!quoteItemData['is_amasty_quote_item'];
+                        for (var i = 0; i < quoteData.items.length; i++) {
+                            if (quoteData.items[i]['is_amasty_quote_item']) {
+                                // isAmastyQuoteItem = quoteDataItem[0]['is_amasty_quote_item'];
+                                isAmastyQuoteItem = true;
+                                break;
+                            }
+                        }
+                        if (isAmastyQuoteItem) {
+                            jQuery('.yotpo-checkout-widget-content').hide();
+                        }
                     }
                 }
-                return isAmastyQuoteItem;
             }
-        };
-        return Component.extend(mixin);
+        });
     }
 );
