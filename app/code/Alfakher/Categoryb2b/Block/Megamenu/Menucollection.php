c474f46497a4efe53749e725db29e5a468125926
Rohit Tripathi <rohit.t@codilar.com>
2023-08-29 17:30:30 +0530

    mega menu strecture chnage for global hookah and design

---
+++ app/code/Alfakher/Categoryb2b/Block/Megamenu/Menucollection.php
@@ -0,0 +1,42 @@
+<?php
+    namespace Alfakher\Categoryb2b\Block\Megamenu;
+
+    class Menucollection extends \Magento\Framework\View\Element\Template{ 
+        protected $_categoryCollection;
+        protected $_storeManager;
+		protected $categoryFactory;
+        
+        public function __construct(
+            \Magento\Cms\Api\BlockRepositoryInterface $blockRepository,
+            \Magento\Framework\View\Element\Template\Context $context,
+            \Magento\Catalog\Helper\Category $categoryHelper,
+			\Magento\Catalog\Model\CategoryFactory $categoryFactory,
+            array $data = []
+        ) {
+            $this->blockRepository = $blockRepository;
+            $this->_categoryHelper = $categoryHelper;
+			$this->categoryFactory = $categoryFactory;
+            parent::__construct($context, $data);
+        }    
+        public function getMainStoreCategories($sorted = false, $asCollection = false, $toLoad = true)
+        {
+            return $this->_categoryHelper->getStoreCategories($sorted , $asCollection, $toLoad);
+        }    
+       
+        public function getAllCurrentStoreCategories($sorted = true, $asCollection = true, $toLoad = true)
+        {
+            return $this->_categoryHelper->getStoreCategories($sorted , $asCollection, $toLoad);
+        }
+        public function getContent($staticBlockId)
+        {
+            $block = $this->blockRepository->getById($staticBlockId);
+            return $block->getContent();
+        }
+
+		public function getCategoryById($categoryId)
+        {
+            return $this->categoryFactory->create()->load($categoryId);
+        }
+
+
+    }
