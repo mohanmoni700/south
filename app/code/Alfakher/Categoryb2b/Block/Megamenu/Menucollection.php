f03d1559f0b41944846c84b7882fb857e16d5660
Rohit Tripathi <rohit.t@codilar.com>
2023-08-29 22:07:51 +0530

    code refacter

--- app/code/Alfakher/Categoryb2b/Block/Megamenu/Menucollection.php
+++ app/code/Alfakher/Categoryb2b/Block/Megamenu/Menucollection.php
@@ -4,18 +4,18 @@
     class Menucollection extends \Magento\Framework\View\Element\Template{
         protected $_categoryCollection;
         protected $_storeManager;
-		protected $categoryFactory;
+        protected $categoryFactory;
 
         public function __construct(
             \Magento\Cms\Api\BlockRepositoryInterface $blockRepository,
             \Magento\Framework\View\Element\Template\Context $context,
             \Magento\Catalog\Helper\Category $categoryHelper,
-			\Magento\Catalog\Model\CategoryFactory $categoryFactory,
+            \Magento\Catalog\Model\CategoryFactory $categoryFactory,
             array $data = []
         ) {
             $this->blockRepository = $blockRepository;
             $this->_categoryHelper = $categoryHelper;
-			$this->categoryFactory = $categoryFactory;
+            $this->categoryFactory = $categoryFactory;
             parent::__construct($context, $data);
         }
         public function getMainStoreCategories($sorted = false, $asCollection = false, $toLoad = true)
@@ -28,10 +28,9 @@
             return $block->getContent();
         }
 
-		public function getCategoryById($categoryId)
+        public function getCategoryById($categoryId)
         {
             return $this->categoryFactory->create()->load($categoryId);
         }
 
-    }
-    
+    }    
