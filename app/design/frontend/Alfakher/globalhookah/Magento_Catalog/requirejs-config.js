93c9594996208f6d82a1e7675fb5228808764394
vinodkumar-codilar <vinodkumar.n@codilar.com>
2023-09-07 14:39:54 +0530

    SSB-205: remobed nav tabs logic

--- app/design/frontend/Alfakher/globalhookah/Magento_Catalog/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-let config = {
-    deps: [
-        "Magento_Catalog/js/product_nav_tabs"
-    ]
-};
