684cda3f9d4f42a3ab8e2929cf22c9e4ef7e7295
Liya.George <liya.george@hookah.com>
2023-09-29 17:00:54 +0530

    SSC-230: sorting for configurable product child products

--- app/code/Alfakher/OutOfStockProduct/Plugin/ProductList.php
+++ app/code/Alfakher/OutOfStockProduct/Plugin/ProductList.php
@@ -4,6 +4,7 @@ namespace Alfakher\OutOfStockProduct\Plugin;
 
 use Magento\Catalog\Block\Product\ListProduct;
 use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
 
 class ProductList
 {
@@ -37,6 +38,19 @@ class ProductList
         if ($isEnabled) {
             // Modify the collection to move out-of-stock products to the bottom
             $result->getSelect()->order('is_in_stock DESC');
+            foreach ($result as $product) {
+                if ($product->getTypeId() == Configurable::TYPE_CODE) {
+                    $childProducts = $product->getTypeInstance()->getUsedProducts($product);
+                    $inStockChildProducts = array_filter($childProducts, function($childProduct) {
+                        return $childProduct->isAvailable();
+                    });
+
+                    if (!empty($inStockChildProducts)) {
+                        $result->removeItemByKey($product->getId());
+                        $result->addItem($product);
+                    }
+                }
+            }
         }
         return $result;
     }
