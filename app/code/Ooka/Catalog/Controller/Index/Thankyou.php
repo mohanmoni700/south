d1af793bf1a6e4a4bb8fac438e48844a1738ef6f
codilar <Sandesh S>
2023-10-06 17:15:45 +0530

    OOKA-514: add thank you button for giftcard email and trigger thank you mail

---
+++ app/code/Ooka/Catalog/Controller/Index/Thankyou.php
@@ -0,0 +1,128 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ooka\Catalog\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Exception\MailException;
+use Magento\Framework\Mail\Template\TransportBuilder;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Sales\Api\OrderItemRepositoryInterface;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Store\Model\StoreManagerInterface;
+
+/**
+ * Thank you class for Trigger third email
+ */
+class Thankyou extends Action
+{
+    private const XML_PATH_GIFTCARD_EMAIL_TEMPLATE = "giftcard/thankyou_giftcardaccount_email/thankyou_template";
+
+    /**
+     * @var TransportBuilder
+     */
+    protected TransportBuilder $transportBuilder;
+    /**
+     * @var ScopeConfigInterface
+     */
+    protected $scopeConfig;
+    /**
+     * @var OrderItemRepositoryInterface
+     */
+    protected $orderItemRepository;
+    /**
+     * @var StoreManagerInterface
+     */
+    protected $storeManager;
+
+    /**
+     * @param Context $context
+     * @param TransportBuilder $transportBuilder
+     * @param ScopeConfigInterface $scopeConfig
+     * @param OrderItemRepositoryInterface $orderItemRepository
+     * @param StoreManagerInterface $storeManager
+     */
+    public function __construct(
+        Context                      $context,
+        TransportBuilder             $transportBuilder,
+        ScopeConfigInterface         $scopeConfig,
+        OrderItemRepositoryInterface $orderItemRepository,
+        StoreManagerInterface        $storeManager
+    ) {
+        parent::__construct($context);
+        $this->transportBuilder = $transportBuilder;
+        $this->scopeConfig = $scopeConfig;
+        $this->orderItemRepositoryInterface = $orderItemRepository;
+        $this->storeManager = $storeManager;
+    }
+
+    /**
+     * Function for getting order item id,store id,sender email and recipientemail
+     */
+    public function execute()
+    {
+        $itemId = $this->getRequest()->getParam('order_item_id');
+        $orderItem = $this->orderItemRepositoryInterface->get($itemId);
+        $storeId = $orderItem->getStoreId();
+        $storeUrl = $this->storeManager->getStore($storeId)->getBaseUrl();
+        $senderName = $orderItem->getProductOptionByCode('giftcard_sender_name');
+        $senderEmail = $orderItem->getProductOptionByCode('giftcard_sender_email');
+        $recipientEmail = $orderItem->getProductOptionByCode('giftcard_recipient_email');
+        $recipientName = $orderItem->getProductOptionByCode('giftcard_recipient_name');
+
+        try {
+
+            $templateId = $this->getGiftcardConfig($storeId);
+            $sender = [
+                'name' => $senderName,
+                'email' => $senderEmail,
+            ];
+            $receiver = [
+                'name' => $recipientName,
+                'email' => $recipientEmail,
+            ];
+            $templateVars = [
+                'name' => 'Recipient Name',
+            ];
+
+            $transport = $this->transportBuilder
+                ->setTemplateIdentifier($templateId)
+                ->setTemplateOptions([
+                    'area' => \Magento\Framework\App\Area::AREA_FRONTEND,
+                    'store' => $storeId,
+                ])
+                ->setTemplateVars($templateVars)
+                ->setFrom($receiver)
+                ->addTo($sender['email'], $receiver['name'])
+                ->getTransport();
+
+            $transport->sendMessage();
+            $this->messageManager->addSuccessMessage('Thank you for your order');
+        } catch (MailException $e) {
+            $this->messageManager->addErrorMessage($e->getMessage());
+            $this->messageManager->addErrorMessage('Email Not Sent');
+        }
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        $resultRedirect->setUrl($storeUrl);
+
+        return $resultRedirect;
+    }
+
+    /**
+     * Function for getting template id
+     *
+     * @param int $storeId
+     * @return mixed
+     */
+    private function getGiftcardConfig($storeId)
+    {
+        return $this->scopeConfig->getValue(
+            self::XML_PATH_GIFTCARD_EMAIL_TEMPLATE,
+            ScopeInterface::SCOPE_STORE,
+            $storeId
+        );
+    }
+}
