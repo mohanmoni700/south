8b6eb5d966e96040ae64aaf1997074a9db104166
sushmahuggi <sushmashuggi@gmail.com>
2023-10-19 12:28:15 +0530

    ssc-180: Cron issue for Product Stock Alert Email

--- app/code/Alfakher/StockAlert/Observer/ProcessBackInStock.php
+++ app/code/Alfakher/StockAlert/Observer/ProcessBackInStock.php
@@ -6,11 +6,17 @@ namespace Alfakher\StockAlert\Observer;
 
 use Alfakher\StockAlert\Model\ResourceModel\ProductAlertStockGuestUser\CollectionFactory as CollectionFactory;
 use Magento\CatalogInventory\Api\StockRegistryInterface as stockRegistry;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\MailException;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Store\Model\ScopeInterface as ScopeInterface;
 use Magento\Store\Model\StoreManagerInterface as StoreManagerInterface;
 use Magento\Framework\App\Config\ScopeConfigInterface as ScopeConfigInterface;
 use Magento\Framework\Mail\Template\TransportBuilder as TransportBuilder;
 use Magento\Framework\Translate\Inline\StateInterface as TranslateStateInterface;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Alfakher\StockAlert\Logger\Logger;
 
 class ProcessBackInStock
 {
@@ -20,14 +26,28 @@ class ProcessBackInStock
     private ScopeConfigInterface $scopeConfig;
     private TransportBuilder $transportBuilder;
     private TranslateStateInterface $inlineTranslation;
+    private ProductRepositoryInterface $productRepository;
+    private Logger $logger;
 
+    /**
+     * @param CollectionFactory $subscriptionCollectionFactory
+     * @param stockRegistry $stockRegistry
+     * @param StoreManagerInterface $storeManager
+     * @param ScopeConfigInterface $scopeConfig
+     * @param TransportBuilder $transportBuilder
+     * @param TranslateStateInterface $inlineTranslation
+     * @param ProductRepositoryInterface $productRepository
+     * @param Logger $logger
+     */
     public function __construct(
         CollectionFactory $subscriptionCollectionFactory,
         stockRegistry $stockRegistry,
         StoreManagerInterface $storeManager,
         ScopeConfigInterface $scopeConfig,
         TransportBuilder $transportBuilder,
-        TranslateStateInterface $inlineTranslation
+        TranslateStateInterface $inlineTranslation,
+        ProductRepositoryInterface $productRepository,
+        Logger $logger
     ) {
         $this->subscriptionCollectionFactory = $subscriptionCollectionFactory;
         $this->stockRegistry = $stockRegistry;
@@ -35,78 +55,131 @@ class ProcessBackInStock
         $this->scopeConfig = $scopeConfig;
         $this->transportBuilder = $transportBuilder;
         $this->inlineTranslation = $inlineTranslation;
+        $this->productRepository = $productRepository;
+        $this->logger = $logger;
     }
 
-    public function execute(\Magento\Framework\Event\Observer $observer)
+    /**
+     * @param Observer $observer
+     * @return void
+     */
+    public function execute(Observer $observer)
     {
+        $this->logger->info("Stock Alert Cron Started for Guest");
         $collection = $this->subscriptionCollectionFactory->create();
         $collection->addFieldToFilter('send_count', 0);
         $subscriptions = $collection->getItems();
         $this->processBackInStock($subscriptions);
     }
 
-    private function processBackInStock($subscriptions): bool
+    /**
+     * @param $subscriptions
+     * @return bool
+     */
+    public function processBackInStock($subscriptions): bool
     {
-        foreach ($subscriptions as $subscription) {
-            $productId = $subscription->getProductId();
-            if ($this->productIsBackInStock($productId)) {
-                $this->sendBackInStockEmail($subscription->getEmail(), $subscription->getProductName());
-                $subscription->setSendDate(date('Y-m-d H:i:s'));
-                $subscription->setSendCount($subscription->getSendCount() + 1);
-                $subscription->setStatus(1);
-                $subscription->save();
+        $collection = $this->subscriptionCollectionFactory->create();
+        $collection->addFieldToFilter('send_count', 0);
+        $subscriptions = $collection->getItems();
+        if ($subscriptions) {
+            foreach ($subscriptions as $subscription) {
+                $productId = (int)$subscription->getData('product_id');
+                $this->logger->info("Product Id back in stock", [
+                    'product_id' => $productId
+                ]);
+                if ($this->productIsBackInStock($productId)) {
+                    $this->sendBackInStockEmail(
+                        $subscription->getData('email_id'),
+                        (int)$subscription->getData('product_id'),
+                        $subscription->getData('name')
+                    );
+                    $subscription->setData('send_date', date('Y-m-d H:i:s'));
+                    $subscription->setData('send_count', (int)$subscription->getData('send_count') + 1);
+                    $subscription->setData('status', 1);
+                    $subscription->save();
+                }
             }
+            return true;
+        } else {
+            return false;
         }
-        return true;
     }
 
-    private function productIsBackInStock(mixed $productId): int
+    /**
+     * @param int $productId
+     */
+    private function productIsBackInStock(int $productId)
     {
         $stockItem = $this->stockRegistry->getStockItem($productId);
         return $stockItem->getIsInStock();
     }
 
-    private function sendBackInStockEmail($email, $productName)
+    /**
+     * @param $email
+     * @param int $productId
+     * @return $this
+     * @throws NoSuchEntityException
+     */
+    private function sendBackInStockEmail($email, int $productId, $customerName)
     {
-        $templateVars = [
-            'productName' => $productName
-        ];
+        try {
+            $product = $this->productRepository->getById($productId);
 
-        $storeId = $this->storeManager->getStore()->getId();
+            $templateVars = [
+                'customerName' => $customerName,
+                'productName' => $product->getName()
+            ];
 
-        $sender =  $this->scopeConfig->getValue(
-            "catalog/productalert/email_identity",
-            ScopeInterface::SCOPE_STORE,
-            $storeId
-        );
+            $storeId = $this->storeManager->getStore()->getId();
 
-        $from = ['email' => $sender, 'name' => $sender];
-        $to = [$email];
+            $fromEmail = $this->scopeConfig->getValue('trans_email/ident_general/email', ScopeInterface::SCOPE_STORE);
+            $fromName = $this->scopeConfig->getValue('trans_email/ident_general/name', ScopeInterface::SCOPE_STORE);
+            $from = ['email' => $fromEmail, 'name' => $fromName];
+            $to = [$email];
 
-        $templateOptions = [
-            'area' => \Magento\Framework\App\Area::AREA_FRONTEND,
-            'store' => $storeId
-        ];
+            $templateOptions = [
+                'area' => \Magento\Framework\App\Area::AREA_FRONTEND,
+                'store' => $storeId
+            ];
 
-        $templateId = $this->scopeConfig->getValue(
-            "catalog/productalert/guest_user_email_template",
-            ScopeInterface::SCOPE_STORE,
-            $storeId
-        );
+            $templateId = $this->scopeConfig->getValue(
+                "catalog/productalert/guest_user_email_template",
+                ScopeInterface::SCOPE_STORE,
+                $storeId
+            );
 
-        $this->inlineTranslation->suspend();
+            $this->inlineTranslation->suspend();
 
-        $transport = $this->transportBuilder->setTemplateIdentifier($templateId)
-            ->setTemplateOptions($templateOptions)
-            ->setTemplateVars($templateVars)
-            ->setFrom($from)
-            ->addTo($to)
-            ->getTransport();
+            $this->logger->info("Email Data", [
+                'customer_name' => $customerName,
+                'product_name' => $product->getName(),
+                'email' => $email,
+                'from_email' => $from,
+                'to_email' => $to,
+                'template_id' => $templateId,
+                'store_id' => $storeId
+            ]);
+            $transport = $this->transportBuilder->setTemplateIdentifier($templateId)
+                ->setTemplateOptions($templateOptions)
+                ->setTemplateVars($templateVars)
+                ->setFrom($from)
+                ->addTo($to)
+                ->getTransport();
 
-        $transport->sendMessage();
+            $transport->sendMessage();
+            $this->logger->info("Email sent successfully");
 
-        $this->inlineTranslation->resume();
-
-        return $this;
+            $this->inlineTranslation->resume();
+            return $this;
+        } catch (NoSuchEntityException $e) {
+            $this->logger->info("Error in product",[
+                'product_id' => $productId
+            ]);
+            throw new NoSuchEntityException(__("Requested product does not exist - %1", $productId));
+        } catch (MailException|LocalizedException $e) {
+            $this->logger->info("Error in email data",[
+                'error' => $e->getMessage()
+            ]);
+        }
     }
 }
